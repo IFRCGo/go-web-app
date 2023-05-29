c77400c1b9296aa5db266403845aa3d6e66d1caf
Aditya Khatri <adityakhatri47@gmail.com>
2023-05-29 11:04:12 +0545

    Add tabs in account page

--- src/views/Account/index.tsx
+++ src/views/Account/index.tsx
@@ -1,17 +1,63 @@
+import { useState } from 'react';
 import Page from '#components/Page';
 import useTranslation from '#hooks/useTranslation';
 
+import Tab from '#components/Tabs/Tab';
+import Tabs from '#components/Tabs';
+import TabList from '#components/Tabs/TabList';
+import TabPanel from '#components/Tabs/TabPanel';
+
 import i18n from './i18n.json';
+
+type Tabs = 'account-info' | 'notifications' | 'per-form' | 'drefs' | '3w-forms';
 
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
     const strings = useTranslation(i18n);
+    const [currentTab, setCurrentTab] = useState<Tabs>('3w-forms');
 
     return (
         <Page
             title={strings.accountTitle}
         >
-            Account Page
+            <Tabs
+                onChange={setCurrentTab}
+                value={currentTab}
+                variant="primary"
+            >
+                <TabList>
+                    <Tab name="account-page">
+                        Account Information
+                    </Tab>
+                    <Tab name="notifications">
+                        Notifications
+                    </Tab>
+                    <Tab name="per-forms">
+                        PER Forms
+                    </Tab>
+                    <Tab name="drefs">
+                        My DREF Applications
+                    </Tab>
+                    <Tab name="3w-forms">
+                        3W Forms
+                    </Tab>
+                </TabList>
+                <TabPanel name="account-page">
+                    Account Information
+                </TabPanel>
+                <TabPanel name="notifications">
+                    Notifications
+                </TabPanel>
+                <TabPanel name="per-forms">
+                    PER Forms
+                </TabPanel>
+                <TabPanel name="drefs">
+                    My DREF Applications
+                </TabPanel>
+                <TabPanel name="3w-forms">
+                    3W Forms go here
+                </TabPanel>
+            </Tabs>
         </Page>
     );
 }
