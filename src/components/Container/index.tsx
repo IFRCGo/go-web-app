e3e8ca74654ba2669fe07c7588e5c1370df05537
Sameer Rai <sameerai2736@gmail.com>
2023-05-29 16:55:13 +0545

    refactor: use Footer component in Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,7 +1,7 @@
 import { _cs } from '@togglecorp/fujs';
 
-import useBasicLayout from '#hooks/useBasicLayout';
 import Header from '#components/Header';
+import Footer from '#components/Footer';
 import { Props as HeadingProps } from '#components/Heading';
 import styles from './styles.module.css';
 
@@ -26,34 +26,23 @@ export interface Props {
 
 function Container(props: Props) {
     const {
-        className,
-        icons,
-        heading,
-        headingLevel,
         actions,
         children,
-        headerDescription,
-        headerDescriptionClassName,
         childrenContainerClassName,
-        footerIcons,
+        className,
+        ellipsizeHeading,
+        footerActions,
+        footerClassName,
         footerContent,
         footerContentClassName,
-        footerClassName,
-        footerActions,
+        footerIcons,
+        headerDescription,
+        headerDescriptionClassName,
+        heading,
+        headingLevel,
+        icons,
         withHeaderBorder,
-        ellipsizeHeading,
     } = props;
-
-    const {
-        containerClassName: footerContainerClassName,
-        content: footer,
-    } = useBasicLayout({
-        icons: footerIcons,
-        children: footerContent,
-        actions: footerActions,
-        childrenContainerClassName: footerContentClassName,
-        className: footerClassName,
-    });
 
     const showFooter = footerIcons || footerContent || footerActions;
 
@@ -77,11 +66,14 @@ function Container(props: Props) {
                 {children}
             </div>
             {showFooter && (
-                <footer
-                    className={_cs(styles.footer, footerContainerClassName)}
+                <Footer
+                    actions={footerActions}
+                    icons={footerIcons}
+                    childrenContainerClassName={footerContentClassName}
+                    className={_cs(styles.footer, footerClassName)}
                 >
-                    {footer}
-                </footer>
+                    {footerContent}
+                </Footer>
             )}
         </div>
     );
