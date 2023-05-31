d495f5708fe80a7d785f56df966a5562dd3b4e79
tnagorra <weathermist@gmail.com>
2023-05-31 19:35:19 +0545

    Pass environment to build
    - Remove FDRS Auth env

--- env.ts
+++ env.ts
@@ -1,16 +1,8 @@
 import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
 
 export default defineConfig({
-    APP_TITLE: Schema.string(),
-    APP_MAPBOX_ACCESS_TOKEN: Schema.string(),
-
-    APP_API_ENDPOINT: Schema.string(),
     APP_ADMIN_URL: Schema.string.optional(),
-    APP_FDRS_AUTH: Schema.string(),
-    APP_RISK_API_ENDPOINT: Schema.string(),
-    APP_RISK_ADMIN_URL: Schema.string.optional(),
-    APP_TINY_API_KEY: Schema.string(),
-    APP_SHOW_ENV_BANNER: Schema.boolean.optional(),
+    APP_API_ENDPOINT: Schema.string(),
     APP_ENVIRONMENT: (key, value) => {
         if (!value) {
             return 'development';
@@ -22,7 +14,13 @@ export default defineConfig({
 
         return value;
     },
+    APP_MAPBOX_ACCESS_TOKEN: Schema.string(),
+    APP_RISK_ADMIN_URL: Schema.string.optional(),
+    APP_RISK_API_ENDPOINT: Schema.string(),
     APP_SENTRY_DSN: Schema.string.optional(),
-    APP_SENTRY_TRACES_SAMPLE_RATE: Schema.number.optional(),
     APP_SENTRY_NORMALIZE_DEPTH: Schema.number.optional(),
+    APP_SENTRY_TRACES_SAMPLE_RATE: Schema.number.optional(),
+    APP_SHOW_ENV_BANNER: Schema.boolean.optional(),
+    APP_TINY_API_KEY: Schema.string(),
+    APP_TITLE: Schema.string(),
 })
