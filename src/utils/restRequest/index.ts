cf8ad81e375fb7d8053a97107d7be02ffecc6a94
tnagorra <weathermist@gmail.com>
2023-07-26 23:35:56 +0545

    Use conditional types instead of function overloading for go request types

--- src/utils/restRequest/index.ts
+++ src/utils/restRequest/index.ts
@@ -1,6 +1,6 @@
 import {
     RequestContext,
-    useRequest,
+    useRequest as useReq,
     useLazyRequest,
     RequestOptions,
     LazyRequestOptions,
@@ -14,23 +14,238 @@ import {
     AdditionalOptions,
 } from './go';
 
-// TODO: add url typing
-// TODO: use path varialbes
-// eslint-disable-next-line max-len
-const useGoLazyRequest: <R, C = unknown>(requestOptions: LazyRequestOptions<R, TransformedError, C, AdditionalOptions>) => {
-    response: R | undefined;
+type GoGetOptions<T extends keyof goApiPaths, OMISSION extends 'response' = never> = {
+    [key in keyof goApiPaths]: goApiPaths[key] extends {
+        get: {
+            parameters: {
+                query?: infer Query,
+                path?: infer Path,
+            },
+            responses: {
+                200: {
+                    content: {
+                        'application/json': infer Response,
+                    },
+                },
+            },
+        },
+    }
+        ? Omit<{
+            url: key,
+            query: Query,
+            pathVariables: Path,
+            response: Response,
+        }, OMISSION>
+        : never
+}[T]
+type GoPutOptions<T extends keyof goApiPaths, OMISSION extends 'response' = never> = {
+    [key in keyof goApiPaths]: goApiPaths[key] extends {
+        put: {
+            parameters: {
+                query?: infer Query,
+                path?: infer Path,
+            },
+            responses: {
+                200: {
+                    content: {
+                        'application/json': infer Response,
+                    },
+                },
+            },
+            requestBody: {
+                content: {
+                    'application/json': infer Body,
+                }
+            },
+        },
+    }
+        ? Omit<{
+            url: key,
+            query: Query,
+            pathVariables: Path,
+            response: Response,
+            body: Body,
+        }, OMISSION>
+        : never
+}[T]
+type GoPatchOptions<T extends keyof goApiPaths, OMISSION extends 'response' = never> = {
+    [key in keyof goApiPaths]: goApiPaths[key] extends {
+        patch: {
+            parameters: {
+                query?: infer Query,
+                path?: infer Path,
+            },
+            responses: {
+                200: {
+                    content: {
+                        'application/json': infer Response,
+                    },
+                },
+            },
+            requestBody: {
+                content: {
+                    'application/json': infer Body,
+                }
+            },
+        },
+    }
+        ? Omit<{
+            url: key,
+            query: Query,
+            pathVariables: Path,
+            response: Response,
+            body: Body,
+        }, OMISSION>
+        : never
+}[T]
+type GoPostOptions<T extends keyof goApiPaths, OMISSION extends 'response' = never> = {
+    [key in keyof goApiPaths]: goApiPaths[key] extends {
+        post: {
+            parameters: {
+                query?: infer Query,
+                path?: infer Path,
+            },
+            responses: {
+                200: {
+                    content: {
+                        'application/json': infer Response,
+                    },
+                },
+            },
+            requestBody: {
+                content: {
+                    'application/json': infer Body,
+                }
+            },
+        },
+    }
+        ? Omit<{
+            url: key,
+            query: Query,
+            pathVariables: Path,
+            response: Response,
+            body: Body,
+        }, OMISSION>
+        : never
+}[T]
+type GoDeleteOptions<T extends keyof goApiPaths> = {
+    [key in keyof goApiPaths]: goApiPaths[key] extends {
+        delete: {
+            parameters: {
+                query?: infer Query,
+                path?: infer Path,
+            },
+        }
+    }
+        ? {
+            url: key,
+            query: Query,
+            pathVariables: Path,
+        }
+        : never
+}[T]
+
+interface BasicGoType<U, M> {
+    url: U,
+    method: M,
+    apiType?: 'default'
+}
+interface BasicGoReturnType<R> {
+    response: R;
     pending: boolean;
     error: TransformedError | undefined;
-    trigger: (ctx: C) => void;
-    context: C | undefined,
-} = useLazyRequest;
+    retrigger: () => void;
+}
+
+// FIXME: Update here
+type GoOptions<T extends keyof goApiPaths, M extends 'get' | 'post' | 'put' | 'patch' | 'delete', OMISSION extends 'response' = never> = (
+    M extends 'get'
+        ? BasicGoType<T, M> & GoGetOptions<T, OMISSION>
+        : M extends 'put'
+            ? BasicGoType<T, M> & GoPutOptions<T, OMISSION>
+            : M extends 'patch'
+                ? BasicGoType<T, M> & GoPatchOptions<T, OMISSION>
+                : M extends 'post'
+                    ? BasicGoType<T, M> & GoPostOptions<T, OMISSION>
+                    : M extends 'delete'
+                        ? BasicGoType<T, M> & GoDeleteOptions<T>
+                        : never
+);
+// FIXME: Update here
+type GoResponse<T extends keyof goApiPaths, M extends 'get' | 'post' | 'put' | 'patch' | 'delete'> = (
+    M extends 'get'
+        ? BasicGoReturnType<GoGetOptions<T>['response']>
+        : M extends 'put'
+            ? BasicGoReturnType<GoPutOptions<T>['response']>
+            : M extends 'patch'
+                ? BasicGoReturnType<GoPatchOptions<T>['response']>
+                : M extends 'post'
+                    ? BasicGoReturnType<GoPostOptions<T>['response']>
+                    : M extends 'delete'
+                        ? BasicGoReturnType<undefined>
+                        : never
+);
+
+function fakeRequest<T extends keyof goApiPaths, M extends 'get' | 'post' | 'put' | 'patch' | 'delete'>(
+    requestOptions: GoOptions<T, M, 'response'>,
+): GoResponse<T, M> {
+    console.warn(requestOptions);
+    return {} as any;
+}
+
+const value = fakeRequest({
+    apiType: 'default',
+    url: '/api/v2/dref-final-report/{id}/',
+    method: 'delete',
+    pathVariables: { id: '12' },
+    query: { format: 'csv' },
+});
+console.warn(value);
+const value2 = fakeRequest<'/api/v2/dref-final-report/{id}/', 'get'>({
+    apiType: 'default' as const,
+    url: '/api/v2/dref-final-report/{id}/',
+    method: 'get',
+    pathVariables: { id: '20' },
+    query: { format: 'csv' },
+});
+console.warn(value2);
+const value3 = fakeRequest({
+    apiType: 'default' as const,
+    url: '/api/v2/dref-final-report/{id}/',
+    method: 'get',
+    pathVariables: { id: '20' },
+    query: { format: 'csv' },
+});
+console.warn(value3);
+const value4 = fakeRequest<'/api/v2/dref-final-report/{id}/', 'put'>({
+    apiType: 'default' as const,
+    url: '/api/v2/dref-final-report/{id}/',
+    method: 'put',
+    pathVariables: { id: '12' },
+    query: {},
+    body: {},
+});
+console.warn(value4);
+const value5 = fakeRequest({
+    apiType: 'default' as const,
+    url: '/api/v2/per-work-plan/{id}/',
+    method: 'put',
+    pathVariables: { id: 12 },
+    query: {},
+    body: {},
+});
+console.warn(value5);
+
+// No need to look below
+
+type GoApiUrlPaths = keyof goApiPaths;
 
 type GoApiResponseWithContent = {
     'responses': {
         '200': {
             'content': {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                'application/json': any;
+                'application/json': unknown;
             }
         }
     }
@@ -39,14 +254,14 @@ type GoApiResponseWithContent = {
 type GoApiUrlQueryWithContent = {
     'parameters': {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        'query': any;
+        'query': object;
     }
 }
 
 type GoApiUrlPathVariablesWithContent = {
     'parameters': {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        'path': any;
+        'path': object;
     }
 }
 
@@ -75,7 +290,6 @@ type RiskApiUrlPathVariablesWithContent = {
     }
 }
 
-type GoApiUrlPaths = keyof goApiPaths;
 type RiskApiUrlPaths = keyof riskApiPaths;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -136,21 +350,32 @@ type GoApiRequestReturnType<URL extends GoApiUrlPaths> = {
     retrigger: () => void;
 }
 
-function useGoRequest<URL extends RiskApiUrlPaths>(
+// TODO: add url typing
+// TODO: use path varialbes
+// eslint-disable-next-line max-len
+const useGoLazyRequest: <R, C = unknown>(requestOptions: LazyRequestOptions<R, TransformedError, C, AdditionalOptions>) => {
+    response: R | undefined;
+    pending: boolean;
+    error: TransformedError | undefined;
+    trigger: (ctx: C) => void;
+    context: C | undefined,
+} = useLazyRequest;
+
+function useReq<URL extends RiskApiUrlPaths>(
     requestOptions: { apiType: 'risk' } & RiskRequestOptions<URL>
 ): RiskApiRequestReturnType<URL>
-function useGoRequest<URL extends GoApiUrlPaths>(
-    requestOptions: { apiType?: never } & GoRequestOptions<URL>
+function useReq<URL extends GoApiUrlPaths>(
+    requestOptions: { apiType?: 'default' } & GoRequestOptions<URL>
 ): GoApiRequestReturnType<URL>
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function useGoRequest(requestOptions: any): unknown {
-    return useRequest(requestOptions);
+function useReq(requestOptions: any) {
+    return useReq(requestOptions);
 }
 
 // const useGoRequest: GoApiRequest & RiskApiRequest = useRequest;
 
 export {
     RequestContext,
-    useGoRequest as useRequest,
+    useReq as useRequest,
     useGoLazyRequest as useLazyRequest,
 };
