9186aacd6379d61c1747c56042bb4ed61c75e67e
barshathakuri <barshathakuri708@gmail.com>
2023-08-07 12:30:48 +0545

    Add new sub page

--- src/views/HealthPSS/index.tsx
+++ src/views/HealthPSS/index.tsx
@@ -1,7 +1,161 @@
+import { useCallback, useContext } from 'react';
+import { generatePath } from 'react-router-dom';
+import { ChevronLeftLineIcon } from '@ifrc-go/icons';
+
+import useTranslation from '#hooks/useTranslation';
+import useGoBack from '#hooks/useGoBack';
+import RouteContext from '#contexts/route';
+import Container from '#components/Container';
+import IconButton from '#components/IconButton';
+import TextOutput from '#components/TextOutput';
+import Link from '#components/Link';
+
+import i18n from './i18n.json';
+import styles from './styles.module.css';
+
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
+    const strings = useTranslation(i18n);
+
+    const {
+        catalogueHealth: catalogueHealthRoute,
+    } = useContext(RouteContext);
+
+    const goBack = useGoBack();
+
+    const handleBackButtonClick = useCallback(() => {
+        goBack(generatePath(catalogueHealthRoute.absolutePath));
+    }, [goBack, catalogueHealthRoute.absolutePath]);
+
     return (
-        <div>Health PSS</div>
+        <Container
+            className={styles.healthPss}
+            heading={strings.healthPSSTitle}
+            childrenContainerClassName={styles.content}
+            icons={(
+                <IconButton
+                    name={undefined}
+                    onClick={handleBackButtonClick}
+                    ariaLabel={strings.healthPSSGoBack}
+                    variant="tertiary"
+                    title={strings.healthPSSGoBack}
+                >
+                    <ChevronLeftLineIcon />
+                </IconButton>
+
+            )}
+            headingLevel={2}
+        >
+            <Container
+                heading={strings.healthPSSCapacityTitle}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.content}
+            >
+                <div>{strings.healthPSSCapacityDetail}</div>
+                <ul>
+                    <li>
+                        {strings.healthPSSCapacityListOne}
+                    </li>
+                    <li>
+                        {strings.healthPSSCapacityListTwo}
+                    </li>
+                    <li>
+                        {strings.healthPSSCapacityListThree}
+                    </li>
+                    <li>
+                        {strings.healthPSSCapacityListFour}
+                    </li>
+                    <li>
+                        {strings.healthPSSCapacityListFive}
+                    </li>
+                    <li>
+                        {strings.healthPSSCapacityListSix}
+                    </li>
+                </ul>
+            </Container>
+            <Container
+                heading={strings.healthPSSDesignedForTitle}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.innerContent}
+            >
+                <div>{strings.healthPSSDesignedForDetail}</div>
+                <div>{strings.healthPSSDesignedForDescription}</div>
+                <ul>
+                    <li>
+                        {strings.healthPSSDesignedForListItemOne}
+                    </li>
+                    <li>
+                        {strings.healthPSSDesignedForListItemTwo}
+                    </li>
+                    <li>
+                        {strings.healthPSSDesignedForListItemThree}
+                    </li>
+                </ul>
+            </Container>
+            <Container
+                heading={strings.healthPSSPersonnel}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.innerContent}
+            >
+                <TextOutput
+                    value={strings.healthPSSPersonnelTotalValue}
+                    label={strings.healthPSSPersonnelTotalLabel}
+                    strongLabel
+                />
+                <TextOutput
+                    value={strings.healthPSSPersonnelCompositionValue}
+                    label={strings.healthPSSPersonnelCompositionLabel}
+                    strongLabel
+                />
+            </Container>
+            <Container
+                heading={strings.healthPSSStandardComponentsLabel}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.innerContent}
+            >
+                <div>{strings.healthPSSStandardComponentsDetail}</div>
+                <ul>
+                    <li>
+                        <TextOutput
+                            value={strings.healthPSSStandardComponentsItemOneValue}
+                            label={strings.healthPSSStandardComponentsItemOneLabel}
+                            strongLabel
+                        />
+                    </li>
+                </ul>
+            </Container>
+            <Container
+                heading={strings.healthPSSSpecificationsTitle}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.innerContent}
+            >
+                <TextOutput
+                    value={strings.healthPSSSpecificationsNsValue}
+                    label={strings.healthPSSSpecificationsNsLabel}
+                    strongLabel
+                />
+            </Container>
+
+            <Container
+                heading={strings.healthPSSAdditionalResources}
+                headingLevel={3}
+                withHeaderBorder
+                childrenContainerClassName={styles.innerContent}
+            >
+                <Link
+                    to="http://pscentre.org/"
+                    variant="tertiary"
+                    withExternalLinkIcon
+                >
+                    {strings.healthPSSAdditionalResourcesLink}
+                </Link>
+            </Container>
+        </Container>
     );
 }
 
