5e8e224a699fa737dfaecbfa8921c51f4b3f7b68
shreeyash07 <shresthashree0807@gmail.com>
2023-08-23 16:48:11 +0545

    Add link list in country additional tab

--- src/views/CountryAdditionalData/index.tsx
+++ src/views/CountryAdditionalData/index.tsx
@@ -9,6 +9,10 @@ import List from '#components/List';
 import Container from '#components/Container';
 import useTranslation from '#hooks/useTranslation';
 import Table from '#components/Table';
+import Link,
+{
+    type Props as LinkProps,
+} from '#components/Link';
 import {
     createStringColumn,
 } from '#components/Table/ColumnShortcuts';
@@ -20,11 +24,13 @@ import HtmlOutput,
 import i18n from './i18n.json';
 import styles from './styles.module.css';
 
-type CountrySnippetType = NonNullable<CountrySnippetResponse['results']>[number]
-type CountryContactsType = NonNullable<CountryResponse['contacts']>[number]
+type CountrySnippetType = NonNullable<CountrySnippetResponse['results']>[number];
+type CountryContactsType = NonNullable<CountryResponse['contacts']>[number];
+type CountryLinksType = NonNullable<CountryResponse['links']>[number];
 
 const contactKeySelector = (contact: CountryContactsType) => String(contact.id);
 const snippetKeySelector = (snippet: CountrySnippetType) => snippet.id;
+const linkKeySelector = (snippet: CountryLinksType) => snippet.id;
 
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
@@ -35,6 +41,9 @@ export function Component() {
     const hasCountryContacts = (countryResponse?.contacts
         && (countryResponse.contacts.length > 0));
 
+    const hasCountryLinks = (countryResponse?.links
+        && (countryResponse.links.length > 0));
+
     const strings = useTranslation(i18n);
     const countrySnippetRendererParams = useCallback(
         (_: number, data: CountrySnippetType): HtmlOutputProps => ({
@@ -44,34 +53,43 @@ export function Component() {
         [],
     );
 
+    const countryLinkRendererParams = useCallback(
+        (_: number, data: CountryLinksType): LinkProps => ({
+            to: data.url,
+            withExternalLinkIcon: true,
+            children: data.title,
+        }),
+        [],
+    );
+
     const contactTableColumns = useMemo(
         () => ([
             createStringColumn<CountryContactsType, string>(
                 'name',
-                'Name',
+                strings.columnName,
                 (item) => item.name,
             ),
             createStringColumn<CountryContactsType, string>(
                 'title',
-                'Title',
+                strings.columnTitle,
                 (item) => item.title,
             ),
             createStringColumn<CountryContactsType, string>(
                 'ctype',
-                'Type',
+                strings.columnType,
                 (item) => item.ctype,
             ),
             createStringColumn<CountryContactsType, string>(
                 'email',
-                'Email',
+                strings.columnEmail,
                 (item) => item.email,
             ),
         ]),
-        [],
+        [strings],
     );
 
     return (
-        <div>
+        <div className={styles.additionalContent}>
             {hasCountrySnippet && (
                 <List
                     data={countrySnippetResponse.results}
@@ -96,6 +114,24 @@ export function Component() {
                         pending={false}
                         keySelector={contactKeySelector}
                         data={countryResponse.contacts}
+                    />
+                </Container>
+            )}
+            {hasCountryLinks && (
+                <Container
+                    withHeaderBorder
+                    heading={strings.linkTitle}
+                >
+                    <List
+                        data={countryResponse.links}
+                        renderer={Link}
+                        rendererParams={countryLinkRendererParams}
+                        keySelector={linkKeySelector}
+                        withoutMessage
+                        compact
+                        pending={false}
+                        errored={false}
+                        filtered={false}
                     />
                 </Container>
             )}
