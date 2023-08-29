55c00f7135f56329eefe07c39ff6f30bf4dad235
Sameer Rai <sameerai2736@gmail.com>
2023-08-29 15:12:18 +0545

    feat: emergency overview in emergency detail page

--- src/views/EmergencyDetails/index.tsx
+++ src/views/EmergencyDetails/index.tsx
@@ -2,11 +2,12 @@ import { useOutletContext } from 'react-router-dom';
 import useTranslation from '#hooks/useTranslation';
 import { isDefined } from '@togglecorp/fujs';
 
-import UnderConstructionMessage from '#components/UnderConstructionMessage';
 import KeyFigure from '#components/KeyFigure';
 import TextOutput from '#components/TextOutput';
 import Container from '#components/Container';
 import type { EmergencyOutletContext } from '#utils/outletContext';
+import useDisasterType from '#hooks/domain/useDisasterType';
+import SeverityIndicator from '#components/domain/SeverityIndicator';
 
 import i18n from './i18n.json';
 import styles from './styles.module.css';
@@ -14,10 +15,28 @@ import styles from './styles.module.css';
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
     const strings = useTranslation(i18n);
+    const disasterTypes = useDisasterType();
     const { emergencyResponse } = useOutletContext<EmergencyOutletContext>();
 
     const hasKeyFigures = isDefined(emergencyResponse)
-        && emergencyResponse.key_figures.length !== 0;
+                       && emergencyResponse.key_figures.length !== 0;
+
+    const disasterType = disasterTypes?.find(
+        (typeOfDisaster) => typeOfDisaster.id === emergencyResponse?.dtype,
+    );
+
+    const mdrCode = isDefined(emergencyResponse)
+        && isDefined(emergencyResponse?.appeals)
+        && emergencyResponse.appeals.length > 0
+        ? emergencyResponse?.appeals[0].code : undefined;
+
+    const hasFieldReports = isDefined(emergencyResponse)
+        && isDefined(emergencyResponse?.field_reports)
+        && emergencyResponse?.field_reports.length > 0;
+
+    const firstFieldReport = hasFieldReports ? emergencyResponse.field_reports[0] : undefined;
+    const assistanceIsRequestedByNS = firstFieldReport?.ns_request_assistance;
+    const assistanceIsRequestedByCountry = firstFieldReport?.request_assistance;
 
     return (
         <div className={styles.emergencyDetails}>
@@ -45,11 +64,65 @@ export function Component() {
                     )}
                 </Container>
             )}
-            {!hasKeyFigures && (
-                <UnderConstructionMessage
-                    title="Emergency Details"
-                />
-            )}
+            <Container
+                heading={strings.emergencyOverviewTitle}
+                withHeaderBorder
+                childrenContainerClassName={styles.overviewContainer}
+            >
+                <div className={styles.overview}>
+                    <TextOutput
+                        label={strings.disasterCategorization}
+                        value={emergencyResponse?.ifrc_severity_level_display}
+                        description={(
+                            <SeverityIndicator
+                                level={isDefined(emergencyResponse?.ifrc_severity_level)
+                                    ? Number(emergencyResponse?.ifrc_severity_level) : null}
+                            />
+                        )}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.disasterType}
+                        value={disasterType?.name}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.GLIDENumber}
+                        value={emergencyResponse?.glide}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.startDate}
+                        valueType="date"
+                        value={emergencyResponse?.disaster_start_date}
+                        strongValue
+                    />
+                </div>
+                <div className={styles.overview}>
+                    <TextOutput
+                        label={strings.visibility}
+                        value={emergencyResponse?.visibility}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.MDRCode}
+                        value={mdrCode}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.assisanceRequestedByNS}
+                        valueType="boolean"
+                        value={assistanceIsRequestedByNS}
+                        strongValue
+                    />
+                    <TextOutput
+                        label={strings.assisanceRequestedByGovernment}
+                        valueType="boolean"
+                        value={assistanceIsRequestedByCountry}
+                        strongValue
+                    />
+                </div>
+            </Container>
         </div>
     );
 }
