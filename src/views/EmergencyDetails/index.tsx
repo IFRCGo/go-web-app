098131d5ea51e91ad7b778b29d4f93dd1ccc8726
Sameer Rai <sameerai2736@gmail.com>
2023-08-30 16:15:33 +0545

    feat: add field report stats to emergency view

--- src/views/EmergencyDetails/index.tsx
+++ src/views/EmergencyDetails/index.tsx
@@ -1,6 +1,8 @@
 import { useOutletContext } from 'react-router-dom';
 import useTranslation from '#hooks/useTranslation';
-import { isDefined, isTruthyString } from '@togglecorp/fujs';
+import {
+    compareDate, isDefined, isNotDefined, isTruthyString,
+} from '@togglecorp/fujs';
 import { DownloadFillIcon } from '@ifrc-go/icons';
 
 import Button from '#components/Button';
@@ -14,11 +16,45 @@ import TextOutput from '#components/TextOutput';
 import Tooltip from '#components/Tooltip';
 import type { EmergencyOutletContext } from '#utils/outletContext';
 import useDisasterType from '#hooks/domain/useDisasterType';
+import type { GoApiResponse } from '#utils/restRequest';
 
 import EmergencyMap from './EmergencyMap';
+import FieldReportStats from './FieldReportStats';
 
 import i18n from './i18n.json';
 import styles from './styles.module.css';
+
+type EventItem = GoApiResponse<'/api/v2/event/{id}'>;
+type FieldReport = EventItem['field_reports'][number];
+
+function getFieldReport(
+    reports: FieldReport[],
+    compareFunction: (
+        a?: string,
+        b?: string,
+        direction?: number
+    ) => number,
+    direction?: number,
+): FieldReport | undefined {
+    if (reports.length === 0) {
+        return undefined;
+    }
+
+    return reports.reduce((
+        selectedReport: FieldReport | undefined,
+        currentReport: FieldReport | undefined,
+    ) => {
+        if (isNotDefined(selectedReport)
+            || compareFunction(
+                currentReport?.updated_at,
+                selectedReport.updated_at,
+                direction,
+            ) > 0) {
+            return currentReport;
+        }
+        return selectedReport;
+    }, undefined);
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
@@ -42,14 +78,18 @@ export function Component() {
         && isDefined(emergencyResponse?.field_reports)
         && emergencyResponse?.field_reports.length > 0;
 
-    const firstFieldReport = hasFieldReports ? emergencyResponse.field_reports[0] : undefined;
+    const firstFieldReport = hasFieldReports
+        ? getFieldReport(emergencyResponse.field_reports, compareDate, -1) : undefined;
     const assistanceIsRequestedByNS = firstFieldReport?.ns_request_assistance;
     const assistanceIsRequestedByCountry = firstFieldReport?.request_assistance;
+    const latestFieldReport = hasFieldReports
+        ? getFieldReport(emergencyResponse.field_reports, compareDate) : undefined;
 
     return (
         <div className={styles.emergencyDetails}>
             {hasKeyFigures && (
                 <Container
+                    className={styles.keyFiguresContainer}
                     heading={strings.emergencyKeyFiguresTitle}
                     childrenContainerClassName={styles.keyFigureList}
                     withHeaderBorder
@@ -169,9 +209,10 @@ export function Component() {
                     ))}
                 </ExpandableContainer>
             )}
-            <div>
-                {emergencyResponse && (
+            <div className={styles.mapKeyFigureContainer}>
+                {emergencyResponse && !emergencyResponse.hide_field_report_map && (
                     <Container
+                        className={styles.mapContainer}
                         heading={strings.emergencyMapTitle}
                         actions={(
                             <Button
@@ -191,6 +232,22 @@ export function Component() {
                         )}
                     </Container>
                 )}
+                {hasFieldReports
+                    && isDefined(latestFieldReport)
+                    && !emergencyResponse.hide_attached_field_reports && (
+                    <Container
+                        className={styles.fieldReportStatsContainer}
+                        heading={strings.emergencyKeyFiguresTitle}
+                        childrenContainerClassName={styles.fieldReportStats}
+                        withHeaderBorder
+                    >
+                        <FieldReportStats
+                            report={latestFieldReport}
+                            disasterType={emergencyResponse.dtype}
+                        />
+                    </Container>
+                )}
+            </div>
         </div>
     );
 }
