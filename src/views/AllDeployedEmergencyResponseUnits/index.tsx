8eee46bd006a9c74bc2b099a73e551a541fb047e
tnagorra <weathermist@gmail.com>
2023-09-01 00:43:38 +0545

    Properly use Links
    - Get eru types from server

--- src/views/AllDeployedEmergencyResponseUnits/index.tsx
+++ src/views/AllDeployedEmergencyResponseUnits/index.tsx
@@ -1,12 +1,11 @@
 import {
     useState,
     useMemo,
-    useContext,
+    useCallback,
 } from 'react';
-import { generatePath } from 'react-router-dom';
 import {
     isNotDefined,
-    isDefined,
+    listToMap,
 } from '@togglecorp/fujs';
 import useTranslation from '#hooks/useTranslation';
 import {
@@ -14,7 +13,6 @@ import {
     useRequest,
 } from '#utils/restRequest';
 import { resolveToComponent } from '#utils/translation';
-import RouteContext from '#contexts/route';
 import Container from '#components/Container';
 import Pager from '#components/Pager';
 import Page from '#components/Page';
@@ -30,36 +28,11 @@ import {
     createNumberColumn,
 } from '#components/Table/ColumnShortcuts';
 import Table from '#components/Table';
+import useGlobalEnums from '#hooks/domain/useGlobalEnums';
 
 import i18n from './i18n.json';
 
-const eruTypes = {
-    0: 'Basecamp',
-    1: 'IT & Telecom',
-    2: 'Logistics',
-    3: 'RCRC Emergency Hospital',
-    4: 'RCRC Emergency Clinic',
-    5: 'Relief',
-    6: 'WASH M15',
-    7: 'WASH MSM20',
-    8: 'WASH M40',
-    9: 'Water Supply and rehabilitation',
-    10: 'Household Water Treatment and safe storage',
-    11: 'Cholera Case management at Community level',
-    12: 'Safe and Dignified Burials',
-    13: 'Community Based Surveillance',
-    14: 'Base Camp – S',
-    15: 'Base Camp – M',
-    16: 'Base Camp – L',
-};
 type EruTableItem = NonNullable<GoApiResponse<'/api/v2/eru/'>['results']>[number];
-
-function getEruType(id: EruTableItem['type']) {
-    if (isNotDefined(id)) {
-        return undefined;
-    }
-    return eruTypes[id];
-}
 
 function keySelector(personnel: EruTableItem) {
     return personnel.id;
@@ -72,10 +45,31 @@ export function Component() {
     const strings = useTranslation(i18n);
     const sortState = useSortState();
     const { sorting } = sortState;
+
     const {
-        country: countryRoute,
-        emergency: emergencyRoute,
-    } = useContext(RouteContext);
+        deployments_eru_type,
+    } = useGlobalEnums();
+
+    const eruTypes = useMemo(
+        () => (
+            listToMap(
+                deployments_eru_type,
+                (item) => item.key,
+                (item) => item.value,
+            )
+        ),
+        [deployments_eru_type],
+    );
+
+    const getEruType = useCallback(
+        (id: EruTableItem['type']) => {
+            if (isNotDefined(id) || isNotDefined(eruTypes)) {
+                return undefined;
+            }
+            return eruTypes[id];
+        },
+        [eruTypes],
+    );
 
     const {
         response: eruResponse,
@@ -98,67 +92,52 @@ export function Component() {
                 strings.eruTableName,
                 (item) => item.eru_owner.national_society_country.society_name
                         ?? item.eru_owner.national_society_country.name,
-                {
-                    sortable: true,
-                },
+                { sortable: true },
             ),
             createStringColumn<EruTableItem, number>(
                 'type',
                 strings.eruTableType,
                 (item) => getEruType(item.type),
-                {
-                    sortable: true,
-                },
+                { sortable: true },
             ),
             createNumberColumn<EruTableItem, number>(
                 'units',
                 strings.eruTablePersonnel,
                 (item) => item.units,
-                {
-                    sortable: true,
-                },
+                { sortable: true },
             ),
             createNumberColumn<EruTableItem, number>(
                 'equipment_units',
                 strings.eruTableEquipment,
                 (item) => item.equipment_units,
-                {
-                    sortable: true,
-                },
+                { sortable: true },
             ),
             createLinkColumn<EruTableItem, number>(
                 'deployed_to__society_name',
                 strings.eruTableCountry,
                 (item) => item.deployed_to.name,
                 (item) => ({
-                    to: isDefined(item.deployed_to?.id)
-                        ? generatePath(countryRoute.absolutePath, {
-                            countryId: item.deployed_to?.id,
-                        })
-                        : undefined,
+                    to: 'countriesLayout',
+                    urlParams: {
+                        countryId: item.deployed_to?.id,
+                    },
                 }),
-                {
-                    sortable: true,
-                },
+                { sortable: true },
             ),
             createLinkColumn<EruTableItem, number>(
                 'event__name',
                 strings.eruTableEmergency,
                 (item) => item.event?.name,
                 (item) => ({
-                    to: isDefined(item.event?.id)
-                        ? generatePath(emergencyRoute.absolutePath, {
-                            emergencyId: item.event?.id,
-                        })
-                        : undefined,
+                    to: 'emergenciesLayout',
+                    urlParams: {
+                        emergencyId: item.event?.id,
+                    },
                 }),
+                { sortable: true },
             ),
         ]),
-        [
-            countryRoute,
-            emergencyRoute,
-            strings,
-        ],
+        [strings, getEruType],
     );
     const containerHeading = resolveToComponent(
         strings.containerHeading,
