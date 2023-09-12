c9dcb1dbebdb0baf5a3ba3739ea3792c311073af
barshathakuri <barshathakuri708@gmail.com>
2023-09-12 11:44:50 +0545

    Fix resource route link

--- src/App/routes/index.tsx
+++ src/App/routes/index.tsx
@@ -2016,7 +2016,7 @@ const accountThreeWForms = customWrapRoute({
 
 const resources = customWrapRoute({
     parent: rootLayout,
-    path: 'about',
+    path: 'resources',
     component: {
         render: () => import('#views/Resources'),
         props: {},
