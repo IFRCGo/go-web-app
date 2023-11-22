1bdcad6173cc78a21921052fb2c489845f1cc466
frozenhelium <ankit.mehta@togglecorp.com>
2023-11-22 14:47:13 +0545

    Restructure country pages
    - Separate out routes into multiple files
    - Add dummy views for new country pages

---
+++ src/views/CountryProfileSeasonalRisks/index.tsx
@@ -0,0 +1,17 @@
+import CountryPageEmptyMessage from '#components/domain/CountryPageEmptyMessage';
+import useTranslation from '#hooks/useTranslation';
+
+import i18n from './i18n.json';
+
+// eslint-disable-next-line import/prefer-default-export
+export function Component() {
+    const strings = useTranslation(i18n);
+
+    return (
+        <CountryPageEmptyMessage
+            title={strings.pageTitle}
+        />
+    );
+}
+
+Component.displayName = 'CountryProfileSeasonalRisks';
