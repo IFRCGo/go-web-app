452a61743a36c424471ed2e01815c1ea1fb5597b
barshathakuri <barshathakuri708@gmail.com>
2023-11-24 17:02:03 +0545

    Add assessment report and supporting document link in the export

--- src/views/DrefApplicationExport/index.tsx
+++ src/views/DrefApplicationExport/index.tsx
@@ -445,6 +445,15 @@ export function Component() {
                             </DescriptionText>
                         </Container>
                     )}
+                    {drefResponse?.supporting_document_details?.file && (
+                        <Link
+                            href={drefResponse?.supporting_document_details?.file}
+                            external
+                            withUnderline
+                        >
+                            {strings.drefApplicationSupportingDocumentation}
+                        </Link>
+                    )}
                 </>
             )}
             {showPreviousOperations && (
@@ -642,6 +651,13 @@ export function Component() {
                             </DescriptionText>
                         </Container>
                     )}
+                    <Link
+                        href={drefResponse?.assessment_report_details?.file}
+                        external
+                        withUnderline
+                    >
+                        {strings.drefAssessmentReportLink}
+                    </Link>
                 </>
             )}
             {showOperationStrategySection && (
