0b31daf87aadbf3cddc3d7906d96c1a8da96a64a
frozenhelium <ankit.mehta@togglecorp.com>
2023-12-04 15:35:45 +0545

    Add strengths and key development priorities in country strategic priority page

--- src/views/CountryNsOverviewContextAndStructure/index.tsx
+++ src/views/CountryNsOverviewContextAndStructure/index.tsx
@@ -1,16 +1,76 @@
-import CountryPageEmptyMessage from '#components/domain/CountryPageEmptyMessage';
+import { useOutletContext } from 'react-router-dom';
+import { isDefined, isTruthyString } from '@togglecorp/fujs';
+
+import Link from '#components/Link';
+import Container from '#components/Container';
 import useTranslation from '#hooks/useTranslation';
+import { type CountryOutletContext } from '#utils/outletContext';
+import { resolveToString } from '#utils/translation';
 
 import i18n from './i18n.json';
+import styles from './styles.module.css';
 
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
+    const { countryResponse } = useOutletContext<CountryOutletContext>();
     const strings = useTranslation(i18n);
 
     return (
-        <CountryPageEmptyMessage
-            title={strings.pageTitle}
-        />
+        <div className={styles.countryNsOverviewContextAndStructure}>
+            {isDefined(countryResponse) && (
+                <Container
+                    heading={strings.keyLinksHeading}
+                    className={styles.keyLinks}
+                    withHeaderBorder
+                >
+                    {isTruthyString(countryResponse.fdrs) && (
+                        <Link
+                            href={`https://data.ifrc.org/FDRS/national-society/${countryResponse.fdrs}`}
+                            external
+                            withLinkIcon
+                        >
+                            {strings.nationalSocietyPageOnFDRS}
+                        </Link>
+                    )}
+                    {isTruthyString(countryResponse.url_ifrc) && (
+                        <Link
+                            href={countryResponse.url_ifrc}
+                            external
+                            withLinkIcon
+                        >
+                            {resolveToString(
+                                strings.countryOnIFRC,
+                                { countryName: countryResponse?.name ?? '-' },
+                            )}
+                        </Link>
+                    )}
+                    {isTruthyString(countryResponse.iso3) && (
+                        <Link
+                            href={`https://reliefweb.int/country/${countryResponse.iso3}`}
+                            external
+                            withLinkIcon
+                        >
+                            {resolveToString(
+                                strings.countryOnReliefWeb,
+                                { countryName: countryResponse?.name ?? '-' },
+                            )}
+                        </Link>
+                    )}
+                    {isTruthyString(countryResponse.society_url) && (
+                        <Link
+                            href={countryResponse?.society_url}
+                            external
+                            withLinkIcon
+                        >
+                            {resolveToString(
+                                strings.countryRCHomepage,
+                                { countryName: countryResponse?.name ?? '-' },
+                            )}
+                        </Link>
+                    )}
+                </Container>
+            )}
+        </div>
     );
 }
 
