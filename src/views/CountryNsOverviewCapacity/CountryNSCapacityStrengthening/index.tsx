6f327f60e8835f69e569b0bdbb849c390efa5569
barshathakuri <barshathakuri708@gmail.com>
2023-12-27 17:36:31 +0545

    Add List view

--- src/views/CountryNsOverviewCapacity/CountryNSCapacityStrengthening/index.tsx
+++ src/views/CountryNsOverviewCapacity/CountryNSCapacityStrengthening/index.tsx
@@ -1,26 +1,32 @@
+import { useCallback } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { ArrowRightUpLineIcon } from '@ifrc-go/icons';
 
 import Container from '#components/Container';
-import Link from '#components/Link';
-import TextOutput from '#components/TextOutput';
-import { type components } from '#generated/types';
+import List from '#components/List';
 import useTranslation from '#hooks/useTranslation';
 import { CountryOutletContext } from '#utils/outletContext';
 
+import CapacityListItem from './CapacityListItem';
 import i18n from './i18n.json';
 import styles from './styles.module.css';
 
-type TypeOfAssessmentEnum = components<'read'>['schemas']['AssessmentTypeEnum'];
-type AssessmentTypeEnum = components<'read'>['schemas']['AssessmentTypeEnum'];
+type CapacityItem = NonNullable<NonNullable<CountryOutletContext['countryResponse']>['capacity']>[number];
 
-const TYPE_OCAC = 0 satisfies AssessmentTypeEnum;
-const TYPE_BOCA = 1 satisfies AssessmentTypeEnum;
+function capacityKeySelector(option: CapacityItem) {
+    return option.id;
+}
 
 function CountryNSCapacityStrengthening() {
     const strings = useTranslation(i18n);
 
     const { countryResponse } = useOutletContext<CountryOutletContext>();
+
+    const rendererParams = useCallback(
+        (_: number, capacity: CapacityItem) => ({
+            capacity,
+        }),
+        [],
+    );
 
     return (
         <div className={styles.countryNsCapacityStrengthening}>
@@ -28,45 +34,18 @@ function CountryNSCapacityStrengthening() {
                 heading={strings.countryNsCapacityStrengtheningHeading}
                 headerDescription={strings.countryNsCapacityStrengtheningDescription}
                 withHeaderBorder
-                contentViewType="grid"
-                numPreferredGridContentColumns={3}
             >
-                {countryResponse?.capacity.map((capacity) => (
-                    <Container
-                        heading={`${capacity.assessment_type_display.toUpperCase()}
-                        ${strings.countryNsCapacityAssessment}`}
-                        headingLevel={4}
-                        withInternalPadding
-                        className={styles.capacityItem}
-                        actions={(
-                            <Link
-                                href={capacity?.url}
-                                variant="primary"
-                                actions={<ArrowRightUpLineIcon />}
-                                external
-                            >
-                                {strings.countryNsCapacityViewDetails}
-                            </Link>
-                        )}
-                    >
-                        {capacity?.assessment_type === TYPE_OCAC && (
-                            <TextOutput
-                                label={strings.countryNsCapacityDateOfAssessment}
-                                value={capacity?.submission_date}
-                                valueType="date"
-                                strongValue
-                            />
-                        )}
-                        {capacity?.assessment_type === TYPE_BOCA && (
-                            <TextOutput
-                                label={strings.countryNsCapacityBranchName}
-                                value={capacity?.branch_name}
-                                valueType="text"
-                                strongValue
-                            />
-                        )}
-                    </Container>
-                ))}
+                <List
+                    className={styles.capacityListContainer}
+                    errored={false}
+                    pending={false}
+                    filtered={false}
+                    data={countryResponse?.capacity}
+                    keySelector={capacityKeySelector}
+                    renderer={CapacityListItem}
+                    rendererParams={rendererParams}
+                    compact
+                />
             </Container>
         </div>
     );
