1c2b1a6038ca1d2796671a52cdda992875940a81
barshathakuri <barshathakuri708@gmail.com>
2023-12-29 12:12:09 +0545

    Add supporting partners contact detail

--- src/views/CountryProfileSupportingPartners/SupportingPartnersContacts/index.tsx
+++ src/views/CountryProfileSupportingPartners/SupportingPartnersContacts/index.tsx
@@ -1,18 +1,27 @@
 import { useMemo } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { _cs } from '@togglecorp/fujs';
+import {
+    _cs,
+    isDefined,
+    isNotDefined,
+    listToGroupList,
+    mapToList,
+} from '@togglecorp/fujs';
 
+import BlockLoading from '#components/BlockLoading';
 import Container from '#components/Container';
 import Table from '#components/Table';
-import useTranslation from '#hooks/useTranslation';
 import { createStringColumn, createLinkColumn } from '#components/Table/ColumnShortcuts';
+import useTranslation from '#hooks/useTranslation';
 import { numericIdSelector } from '#utils/selectors';
+import { GoApiResponse, useRequest } from '#utils/restRequest';
 import { type CountryOutletContext } from '#utils/outletContext';
 
 import i18n from './i18n.json';
 import styles from './styles.module.css';
 
-type ContactListItem = NonNullable<NonNullable<CountryOutletContext['countryResponse']>['contacts']>[number];
+type GetPartnerResponse = GoApiResponse<'/api/v2/country-supporting-partner/'>;
+type ContactListItem = NonNullable<GetPartnerResponse['results']>[number];
 
 interface Props {
     className?: string;
@@ -22,22 +31,46 @@ function SupportingPartnersContacts(props: Props) {
     const { className } = props;
     const strings = useTranslation(i18n);
 
-    const { countryResponse } = useOutletContext<CountryOutletContext>();
+    const { countryId } = useOutletContext<CountryOutletContext>();
+
+    const {
+        pending: contactPending,
+        response: contactResponse,
+    } = useRequest({
+        url: '/api/v2/country-supporting-partner/',
+        skip: isNotDefined(countryId),
+        query: {
+            country: isDefined(countryId) ? [Number(countryId)] : undefined,
+        },
+    });
+
+    const groupedBySupportingType = (
+        listToGroupList(
+            contactResponse?.results,
+            (item) => item.supporting_type_display,
+            (item) => item,
+        ) ?? {}
+    );
+
+    const groupedSupportingList = mapToList(
+        groupedBySupportingType,
+        (d, k) => ({ label: k, value: d }),
+    );
 
     const columns = useMemo(
         () => ([
             createStringColumn<ContactListItem, number>(
-                'name',
+                'first_name',
                 '',
-                (item) => item.name,
+                (item) => (`${item.first_name} ${item.last_name}`),
                 {
                     cellRendererClassName: styles.name,
                 },
             ),
             createStringColumn<ContactListItem, number>(
-                'title',
+                'position',
                 '',
-                (item) => item.title,
+                (item) => item.position,
             ),
             createLinkColumn<ContactListItem, number>(
                 'email',
@@ -59,15 +92,20 @@ function SupportingPartnersContacts(props: Props) {
             heading={strings.supportingPartnersContactsTitle}
             withHeaderBorder
         >
-            <Table
-                className={styles.table}
-                columns={columns}
-                data={countryResponse?.contacts}
-                headersHidden
-                filtered={false}
-                keySelector={numericIdSelector}
-                pending={false}
-            />
+            {contactPending && <BlockLoading className={styles.loading} />}
+            {groupedSupportingList?.map((support) => (
+                <Table
+                    className={styles.table}
+                    caption={support.label.toUpperCase()}
+                    captionClassName={styles.caption}
+                    columns={columns}
+                    data={support.value}
+                    headersHidden
+                    filtered={false}
+                    keySelector={numericIdSelector}
+                    pending={false}
+                />
+            ))}
         </Container>
     );
 }
