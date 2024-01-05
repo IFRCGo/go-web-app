4be584bfec2860e2d9a0ecf6ccf97b20e734bd7b
barshathakuri <barshathakuri708@gmail.com>
2024-01-05 18:14:02 +0545

    Add ifrc presence items

---
+++ src/views/CountryProfileSupportingPartners/PresenceItem/index.tsx
@@ -0,0 +1,88 @@
+import { useOutletContext } from 'react-router-dom';
+
+import Container from '#components/Container';
+import TextOutput from '#components/TextOutput';
+import Link from '#components/Link';
+import useTranslation from '#hooks/useTranslation';
+import { getCurrentMonthYear } from '#utils/common';
+import { type CountryOutletContext } from '#utils/outletContext';
+import { resolveToString } from '#utils/translation';
+
+import i18n from './i18n.json';
+import styles from './styles.module.css';
+
+function PresenceItem() {
+    const strings = useTranslation(i18n);
+
+    const { countryResponse } = useOutletContext<CountryOutletContext>();
+    const presence = countryResponse?.icrc_presence;
+
+    return (
+        <div className={styles.presenceList}>
+            <Container
+                className={styles.presenceCard}
+                childrenContainerClassName={styles.presenceCardList}
+                heading={strings.countryIFRCPresenceTitle}
+            >
+                <div className={styles.ifrcPresenceItem}>
+                    <TextOutput
+                        label={strings.countryIFRCPresenceHeadOfDelegation}
+                        value={undefined}
+                    />
+                    {/* //TODO: Add status Link */}
+                    <Link
+                        href={undefined}
+                        external
+                        variant="tertiary"
+                        withUnderline
+                    >
+                        {strings.countryIFRCLegalStatus}
+                    </Link>
+                </div>
+                <div className={styles.ifrcPresenceItem}>
+                    <TextOutput
+                        label={strings.countryIFRCContact}
+                        value={undefined}
+                    />
+                    <Link
+                        href={countryResponse?.disaster_law_url}
+                        external
+                        variant="tertiary"
+                        withUnderline
+                    >
+                        {strings.countryIFRCDisasterLaw}
+                    </Link>
+                </div>
+            </Container>
+            {presence?.map((icrc) => (
+                <Container
+                    className={styles.presenceCard}
+                    heading={strings.countryICRCPresenceTitle}
+                    childrenContainerClassName={styles.presenceCardList}
+                >
+                    <div>
+                        {strings.countryICRCConfirmedPartner}
+                        {getCurrentMonthYear()}
+                    </div>
+                    <div className={styles.icrcPresenceItem}>
+                        <Link
+                            key={icrc.id}
+                            href={icrc.url}
+                            external
+                            variant="tertiary"
+                            withUnderline
+                        >
+                            {strings.countryICRCKeyOperations}
+                        </Link>
+                        {resolveToString(
+                            strings.countryICRCWithin,
+                            { name: countryResponse?.name ?? '--' },
+                        )}
+                    </div>
+                </Container>
+            ))}
+        </div>
+    );
+}
+
+export default PresenceItem;
