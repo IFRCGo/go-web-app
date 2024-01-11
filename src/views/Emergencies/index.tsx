b37ee3e899a9eaf9d1734eb4981db992f96ef77a
roshni73 <roshni73@gmail.com>
2024-01-11 14:18:01 +0545

    Added  missing alert type  in the event details for GDACS

--- src/views/Emergencies/index.tsx
+++ src/views/Emergencies/index.tsx
@@ -101,7 +101,7 @@ export function Component() {
         query: {
             model_type: 'event',
             unit: 'month',
-            start_date: encodeDate(oneYearAgo),
+            start_date: encodeDate(thirtyDaysAgo),
         } as never,
     });
 
