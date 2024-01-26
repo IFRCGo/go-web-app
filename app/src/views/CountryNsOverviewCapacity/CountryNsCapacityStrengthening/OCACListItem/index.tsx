4248ab100c684f0b1d034fef03f3b9d090bcd85d
samshara <sameerai2736@gmail.com>
2024-01-26 17:24:21 +0545

    fix: BOCA assessment and fund_type in capacity item for national society

---
+++ app/src/views/CountryNsOverviewCapacity/CountryNsCapacityStrengthening/OCACListItem/index.tsx
@@ -0,0 +1,61 @@
+import { ArrowRightUpLineIcon } from '@ifrc-go/icons';
+import {
+    Container,
+    DateOutput,
+} from '@ifrc-go/ui';
+import { useTranslation } from '@ifrc-go/ui/hooks';
+
+import Link from '#components/Link';
+import { CountryOutletContext } from '#utils/outletContext';
+
+import i18n from './i18n.json';
+import styles from './styles.module.css';
+
+export type CapacityItem = NonNullable<NonNullable<CountryOutletContext['countryResponse']>['capacity']>[number];
+
+interface Props {
+    capacity: CapacityItem;
+}
+
+function OCACListItem(props: Props) {
+    const {
+        capacity,
+    } = props;
+
+    const strings = useTranslation(i18n);
+
+    return (
+        <Container
+            className={styles.ocacListItem}
+            heading={
+                `${capacity.assessment_type_display.toUpperCase()}
+                ${strings.capacityListItemAssessment}`
+            }
+            headingLevel={4}
+            withInternalPadding
+            actions={(
+                <Link
+                    href={capacity?.url}
+                    variant="primary"
+                    actions={<ArrowRightUpLineIcon />}
+                    external
+                >
+                    {strings.capacityListItemViewDetails}
+                </Link>
+            )}
+        >
+            <div className={styles.ocacDetail}>
+                <DateOutput
+                    className={styles.date}
+                    format="MMM yyyy"
+                    value={capacity.submission_date}
+                />
+                <div className={styles.label}>
+                    {strings.capacityListItemDateOfAssessment}
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default OCACListItem;
