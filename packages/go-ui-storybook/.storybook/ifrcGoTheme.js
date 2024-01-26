803041f01a33ec40c22ddd43a4456bfa4f0f53f4
samshara <sameerai2736@gmail.com>
2024-01-26 14:56:16 +0545

    feat: implement storybook integration for @ifrc-go/ui

---
+++ packages/go-ui-storybook/.storybook/ifrcGoTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'IFRC-GO',
+    brandUrl: 'https://go.ifrc.org/',
+    brandImage: '/favicon.svg',
+    brandTarget: '_self',
+});
