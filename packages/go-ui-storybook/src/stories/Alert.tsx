35e2e9c6913eec2b7b087ae6112abad5ca665bb3
roshni73 <roshni73@gmail.com>
2024-02-14 11:54:07 +0545

    Add Stories for alert component

---
+++ packages/go-ui-storybook/src/stories/Alert.tsx
@@ -0,0 +1,14 @@
+import {
+    Alert as PureAlert,
+    AlertProps as PureAlertProps,
+} from '@ifrc-go/ui';
+
+interface AlertProps<N extends string> extends PureAlertProps<N> {}
+
+function WrappedAlert<N extends string>(props: AlertProps<N>) {
+    return (
+        <PureAlert {...props} />// eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedAlert;
