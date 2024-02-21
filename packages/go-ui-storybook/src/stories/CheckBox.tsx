eb3fef4b0675dfa9d467938dadf8901e2043acfe
roshni73 <roshni73@gmail.com>
2024-02-21 14:05:46 +0545

    Add Checkbox Story for Checkbox component

---
+++ packages/go-ui-storybook/src/stories/CheckBox.tsx
@@ -0,0 +1,15 @@
+import {
+    Checkbox as PureCheckbox,
+    CheckboxProps as PureCheckboxProps,
+} from '@ifrc-go/ui';
+
+interface CheckboxProps<N> extends PureCheckboxProps<N> {}
+
+function WrappedCheckBox<const N>(props: CheckboxProps<N>) {
+    return (
+        // eslint-disable-next-line max-len
+        <PureCheckbox {...props} /> // eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedCheckBox;
