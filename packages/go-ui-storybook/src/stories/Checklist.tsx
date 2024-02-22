1c5681f033f7aac0f78ab1f6efb1f5114c4c01aa
roshni73 <roshni73@gmail.com>
2024-02-22 10:01:14 +0545

    Add Checklist story for Checklist component

---
+++ packages/go-ui-storybook/src/stories/Checklist.tsx
@@ -0,0 +1,18 @@
+import {
+    Checklist as PureChecklist,
+    ChecklistProps as PureChecklistProps,
+    ListKey,
+} from '@ifrc-go/ui';
+
+interface ChecklistProps<KEY extends ListKey, NAME, OPTION extends object>
+extends PureChecklistProps<KEY, NAME, OPTION> {}
+
+function WrappedChecklist
+<KEY extends ListKey, NAME, OPTION extends object>(props: ChecklistProps<KEY, NAME, OPTION>) {
+    return (
+
+        <PureChecklist {...props} /> // eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedChecklist;
