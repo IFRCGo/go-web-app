32336a2ed3eddb1ca5c85b4013ef9231f3824823
roshni73 <roshni73@gmail.com>
2024-02-28 11:43:36 +0545

    Add TextArea story for TextArea component

---
+++ packages/go-ui-storybook/src/stories/TextArea.stories.tsx
@@ -0,0 +1,100 @@
+import { TextAreaProps } from '@ifrc-go/ui';
+import { useArgs } from '@storybook/preview-api';
+import type {
+    Meta,
+    StoryObj,
+} from '@storybook/react';
+
+import TextArea from './TextArea';
+
+type TextAreaSpecificProps = TextAreaProps<string>;
+
+type Story = StoryObj< TextAreaSpecificProps>;
+
+const meta: Meta<typeof TextArea> = {
+    title: 'Components/ TextArea',
+    component: TextArea,
+    parameters: {
+        layout: 'centered',
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
+        },
+    },
+    tags: ['autodocs'],
+    decorators: [
+        function Component(_, ctx) {
+            const [
+                { value },
+                setArgs,
+            ] = useArgs<{ value: string | undefined }>();
+            const onChange = (val: string | undefined, name: string) => {
+                setArgs({ value: val });
+                ctx.args.onChange(val, name);
+            };
+
+            return (
+                <TextArea
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...ctx.args}
+                    onChange={onChange}
+                    value={value}
+                    name="TextArea"
+                />
+            );
+        },
+    ],
+};
+
+export default meta;
+
+export const Default: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+    },
+};
+export const Disabled: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        disabled: true,
+    },
+};
+export const Error: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        error: 'This is error',
+    },
+};
+export const Hint: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        hint: 'This is a hint',
+    },
+};
+export const ReadOnly: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        readOnly: true,
+    },
+};
+export const WithAsterisk: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        withAsterisk: true,
+    },
+};
+
+export const ErrorOnTooltip: Story = {
+    args: {
+        label: 'Text Area',
+        value: 'This is text area',
+        error: 'This is an error message',
+        errorOnTooltip: true,
+    },
+};
