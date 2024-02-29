b43a515dceb65f5922d296a45384fdfdb54f148a
roshni73 <roshni73@gmail.com>
2024-02-29 11:42:57 +0545

    Add SegmentInput story for SegmentInput component

---
+++ packages/go-ui-storybook/src/stories/SegmentInput.tsx
@@ -0,0 +1,16 @@
+import {
+    RadioProps,
+    SegmentInput as PureSegmentInput,
+    SegmentInputProps as PureSegmentInputProps,
+} from '@ifrc-go/ui';
+
+// eslint-disable-next-line max-len
+function WrappedSegmentInput<const N, O extends object, V
+extends string | number | boolean, RRP extends RadioProps<V, N>>(
+    props: PureSegmentInputProps<N, O, V, RRP>,
+) {
+    return (
+        <PureSegmentInput {...props} /> // eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+export default WrappedSegmentInput;
