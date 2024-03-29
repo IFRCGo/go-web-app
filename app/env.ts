d705be595a1b5eb2da1c1811cb09c758ad93bb5f
samshara <sameerai2736@gmail.com>
2024-03-29 17:03:26 +0545

    feat: update README

--- app/env.ts
+++ app/env.ts
@@ -1,14 +1,14 @@
 import { defineConfig, Schema } from '@julr/vite-plugin-validate-env';
 
 export default defineConfig({
-    APP_ADMIN_URL: Schema.string.optional({ format: 'url', protocol: true }),
-    APP_API_ENDPOINT: Schema.string({ format: 'url', protocol: true }),
-    APP_ENVIRONMENT: Schema.enum(['development', 'testing', 'staging', 'production'] as const),
-    APP_MAPBOX_ACCESS_TOKEN: Schema.string(),
-    APP_RISK_API_ENDPOINT: Schema.string({ format: 'url', protocol: true }),
-    APP_SHOW_ENV_BANNER: Schema.boolean.optional(),
-    APP_TINY_API_KEY: Schema.string(),
     APP_TITLE: Schema.string(),
+    APP_ENVIRONMENT: Schema.enum(['development', 'testing', 'staging', 'production'] as const),
+    APP_API_ENDPOINT: Schema.string({ format: 'url', protocol: true }),
+    APP_ADMIN_URL: Schema.string.optional({ format: 'url', protocol: true }),
+    APP_SHOW_ENV_BANNER: Schema.boolean.optional(),
+    APP_MAPBOX_ACCESS_TOKEN: Schema.string(),
+    APP_TINY_API_KEY: Schema.string(),
+    APP_RISK_API_ENDPOINT: Schema.string({ format: 'url', protocol: true }),
     APP_SENTRY_DSN: Schema.string.optional(),
     APP_SENTRY_TRACES_SAMPLE_RATE: Schema.number.optional(),
     APP_SENTRY_REPLAYS_SESSION_SAMPLE_RATE: Schema.number.optional(),
