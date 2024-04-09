9ba350d6f4fb93c9a810515c1dfbce3eee3ff5cb
samshara <sameerai2736@gmail.com>
2024-04-09 11:35:48 +0545

    feat: empty build directory before building the app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -62,6 +62,7 @@ export default defineConfig(({ mode }) => {
         build: {
             outDir: '../build',
             sourcemap: isProd,
+            emptyOutDir: true,
             rollupOptions: {
                 output: {
                     chunkFileNames: `${env.npm_package_version}/chunk-[name].[hash].js`,
