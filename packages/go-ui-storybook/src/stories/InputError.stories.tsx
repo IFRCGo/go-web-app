f5ef0d4f2d573c20e60df7272bb9eef3f08b89ee
roshni73 <roshnipoudel73@gmail.com>
2024-05-14 10:36:55 +0545

    Add InputError Story for InputError Component

---
+++ packages/go-ui-storybook/src/stories/InputError.stories.tsx
@@ -0,0 +1,39 @@
+import { InputErrorProps } from '@ifrc-go/ui';
+import type {
+    Meta,
+    StoryObj,
+} from '@storybook/react';
+
+import InputError from './InputError';
+
+type InputErrorSpecificProps = InputErrorProps;
+type Story = StoryObj<InputErrorSpecificProps>;
+
+const meta: Meta<typeof InputError> = {
+    title: 'Components/InputError',
+    component: InputError,
+    parameters: {
+        layout: 'centered',
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
+        },
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Default: Story = {
+    args: {
+        children: 'This is an error',
+        className: 'error',
+    },
+};
+export const Disabled: Story = {
+    args: {
+        children: 'This is an error ',
+        className: 'Error',
+        disabled: true,
+    },
+};
