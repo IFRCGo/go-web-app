88e1ee31c2c77e5eb7e151d37cb838feaf1b075a
roshni73 <roshnipoudel73@gmail.com>
2024-05-16 17:17:17 +0545

    Add RawFileInput Story

---
+++ packages/go-ui-storybook/src/stories/RawFileInput.tsx
@@ -0,0 +1,17 @@
+import {
+    RawFileInput as PureRawFileInput,
+    RawFileInputProps as PureRawFileInputProps,
+} from '@ifrc-go/ui';
+
+type RawFileInputProps<N> = PureRawFileInputProps<N>;
+
+function WrappedRawFileInput< const N>(props: RawFileInputProps<N>) {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <PureRawFileInput {...props} />
+
+    );
+}
+
+export default WrappedRawFileInput;
