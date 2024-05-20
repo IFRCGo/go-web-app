0d48596d70fdf1daf7ca76e2c04fa0821c061e15
samshara <sameerai2736@gmail.com>
2024-05-20 15:38:44 +0545

    feat: add commands to run playwright

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig, devices } from "@playwright/test";
-import "dotenv/config";
+import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    testDir: "./tests",
+    testDir: './tests',
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,31 +21,31 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: process.env.APP_BASE_URL,
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: "on-first-retry",
+        trace: 'on-first-retry',
     },
 
     /* Configure projects for major browsers */
     projects: process.env.CI
         ? [
               {
-                  name: "chromium",
-                  use: { ...devices["Desktop Chrome"] },
+                  name: 'chromium',
+                  use: { ...devices['Desktop Chrome'] },
               },
 
               {
-                  name: "firefox",
-                  use: { ...devices["Desktop Firefox"] },
+                  name: 'firefox',
+                  use: { ...devices['Desktop Firefox'] },
               },
 
               {
-                  name: "webkit",
-                  use: { ...devices["Desktop Safari"] },
+                  name: 'webkit',
+                  use: { ...devices['Desktop Safari'] },
               },
 
               /* Test against mobile viewports. */
@@ -71,13 +71,13 @@ export default defineConfig({
         : [
               // NOTE: WebKit is only supported on Debian and Ubuntu in Playwright.
               {
-                  name: "chromium",
-                  use: { ...devices["Desktop Chrome"] },
+                  name: 'chromium',
+                  use: { ...devices['Desktop Chrome'] },
               },
 
               {
-                  name: "firefox",
-                  use: { ...devices["Desktop Firefox"] },
+                  name: 'firefox',
+                  use: { ...devices['Desktop Firefox'] },
               },
           ],
 
