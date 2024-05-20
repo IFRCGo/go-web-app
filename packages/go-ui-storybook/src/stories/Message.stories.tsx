bd3c08672636f25e863f6abf8f8a618105c6febc
roshni73 <roshnipoudel73@gmail.com>
2024-05-20 12:15:46 +0545

    Add Message story

---
+++ packages/go-ui-storybook/src/stories/Message.stories.tsx
@@ -0,0 +1,67 @@
+import { MessageProps } from '@ifrc-go/ui';
+import type {
+    Meta,
+    StoryObj,
+} from '@storybook/react';
+
+import Message from './Message';
+
+type MessageSpecificProps = MessageProps;
+
+type Story = StoryObj<MessageSpecificProps>;
+
+const meta: Meta<typeof Message> = {
+    title: 'Components/Message',
+    component: Message,
+    parameters: {
+        layout: 'centered',
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
+        },
+    },
+    tags: ['autodocs'],
+    decorators: [
+        function Component(_, ctx) {
+            const componentArgs = ctx.args as MessageSpecificProps;
+            return (
+                <Message
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    {...componentArgs}
+
+                />
+            );
+        },
+    ],
+};
+
+export default meta;
+
+export const Default: Story = {
+    args: {
+        title: 'IFRC supported Operation',
+    },
+};
+
+export const WithDescription: Story = {
+    args: {
+        title: 'IFRC supported Operation',
+        description: 'If operation does not appear in the dropdown, the operation does not yet exist in GO. In that case, please submit a new Field Report to generate the operation, then come back to this form',
+    },
+};
+
+export const Info :Story = {
+    args: {
+        title: 'IFRC supported Operation',
+        description: 'If operation does not appear in the dropdown, the operation does not yet exist in GO. In that case, please submit a new Field Report to generate the operation, then come back to this form',
+        variant: 'info',
+
+    },
+};
+
+export const Error :Story = {
+    args: {
+        title: 'Page not found',
+        variant: 'error',
+    },
+};
