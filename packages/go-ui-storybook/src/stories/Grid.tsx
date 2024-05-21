b90ece7e0d9976f9d2160c78544a8745decfe680
roshni73 <roshnipoudel73@gmail.com>
2024-05-21 14:37:43 +0545

    Add Grid  Story

---
+++ packages/go-ui-storybook/src/stories/Grid.tsx
@@ -0,0 +1,18 @@
+import {
+    Grid as PureGrid,
+    GridProps as PureGridProps,
+    ListKey,
+} from '@ifrc-go/ui';
+
+interface GridProps< DATUM, KEY extends ListKey, RENDERER_PROPS>
+extends PureGridProps< DATUM, KEY, RENDERER_PROPS> {}
+
+function WrappedGrid
+// eslint-disable-next-line max-len
+<DATUM, KEY extends ListKey, RENDERER_PROPS>(props: GridProps<DATUM, KEY, RENDERER_PROPS>) {
+    return (
+        <PureGrid {...props} />// eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedGrid;
