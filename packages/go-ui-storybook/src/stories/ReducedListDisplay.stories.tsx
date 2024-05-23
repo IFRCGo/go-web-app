11d7395922a83ffefb3414049da7403ab2619f1b
roshni73 <roshnipoudel73@gmail.com>
2024-05-23 16:23:59 +0545

    Add ReducedListDisplay  story for ReducedListDisplay Component

---
+++ packages/go-ui-storybook/src/stories/ReducedListDisplay.stories.tsx
@@ -0,0 +1,86 @@
+import { ReducedListDisplayProps } from '@ifrc-go/ui';
+import type {
+    Meta,
+    StoryObj,
+} from '@storybook/react';
+
+import ReducedListDisplay from './ReducedListDisplay';
+
+interface Option {
+    label: string;
+}
+
+const options: Option[] = [
+    { label: 'Green' },
+    { label: 'Yellow' },
+    { label: 'Blue' },
+    { label: 'White' },
+    { label: 'Black' },
+];
+
+type RendererProps = {
+    children: React.ReactNode
+};
+
+type ReducedListDisplaySpecificProps = ReducedListDisplayProps<Option, RendererProps>;
+
+type Story = StoryObj<ReducedListDisplaySpecificProps>;
+
+const meta: Meta<typeof ReducedListDisplay> = {
+    title: 'Components/ReducedListDisplay',
+    component: ReducedListDisplay,
+    parameters: {
+        layout: 'centered',
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
+        },
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+function Option({ children }: RendererProps) {
+    return (
+        <div>
+            { children }
+        </div>
+    );
+}
+
+const keySelector = (d: Option) => d.label;
+
+const rendererParams = (option: Option) => ({ children: option.label });
+
+export const Default: Story = {
+    args: {
+        title: 'Test',
+        list: options,
+        keySelector,
+        renderer: Option,
+        rendererParams,
+    },
+};
+
+export const MaxItems: Story = {
+    args: {
+        title: 'Test',
+        list: options,
+        keySelector,
+        renderer: Option,
+        rendererParams,
+        maxItems: 2,
+    },
+};
+
+export const MinItems: Story = {
+    args: {
+        title: 'Test',
+        list: options,
+        keySelector,
+        renderer: Option,
+        rendererParams,
+        minItems: 1,
+    },
+};
