b7eff2b8d8d3dc63417f9f5318f1985aa9579da7
roshni73 <roshnipoudel73@gmail.com>
2024-05-23 16:23:59 +0545

    Add ReducedListDisplay  story for ReducedListDisplay Component

---
+++ packages/go-ui-storybook/src/stories/ReducedListDisplay.tsx
@@ -0,0 +1,17 @@
+import {
+    ReducedListDisplay as PureReducedListDisplay,
+    ReducedListDisplayProps as PureReducedListDisplayProps,
+} from '@ifrc-go/ui';
+
+// eslint-disable-next-line max-len
+type ReducedListDisplayProps<LIST_ITEM, RENDERER_PROPS> = PureReducedListDisplayProps<LIST_ITEM, RENDERER_PROPS>
+
+function WrappedReducedListDisplay
+// eslint-disable-next-line max-len
+<LIST_ITEM, RENDERER_PROPS>(props: ReducedListDisplayProps<LIST_ITEM, RENDERER_PROPS>) {
+    return (
+        <PureReducedListDisplay {...props} />// eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedReducedListDisplay;
