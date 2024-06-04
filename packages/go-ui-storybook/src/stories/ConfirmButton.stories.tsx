d777fafa34aa92800e36a68e72567e80988cd108
samshara <sameerai2736@gmail.com>
2024-06-04 15:15:39 +0545

    fix: use storybook template to write ConfirmButton story

--- packages/go-ui-storybook/src/stories/ConfirmButton.stories.tsx
+++ packages/go-ui-storybook/src/stories/ConfirmButton.stories.tsx
@@ -1,17 +1,17 @@
-import { ConfirmButtonProps } from '@ifrc-go/ui';
+import { useArgs } from '@storybook/preview-api';
 import type {
+    Args,
     Meta,
     StoryObj,
 } from '@storybook/react';
+import { fn } from '@storybook/test';
 
 import ConfirmButton from './ConfirmButton';
 
-type ConfirmButtonSpecificProps = ConfirmButtonProps<string>;
-
-type Story = StoryObj< ConfirmButtonSpecificProps>;
+type Story = StoryObj<typeof ConfirmButton>;
 
 const meta: Meta<typeof ConfirmButton> = {
-    title: 'Components/ ConfirmButton',
+    title: 'Components/ConfirmButton',
     component: ConfirmButton,
     parameters: {
         layout: 'centered',
@@ -20,35 +20,62 @@ const meta: Meta<typeof ConfirmButton> = {
             url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
         },
     },
+    args: {
+        onClick: fn(),
+        onConfirm: fn(),
+    },
     tags: ['autodocs'],
-    decorators: [
-        function Component(_, ctx) {
-            const componentArgs = ctx.args as ConfirmButtonSpecificProps;
-            return (
-                <ConfirmButton
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    {...componentArgs}
-                />
-            );
-        },
-    ],
 };
 
 export default meta;
 
-export const Default: Story = {
+function Template(args:Args) {
+    const [
+        {
+            onConfirm,
+        },
+    ] = useArgs();
+
+    const handleConfirm = (name: string) => {
+        onConfirm(name);
+    };
+
+    return (
+        <ConfirmButton
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...args}
+            name="confirmButton"
+            confirmMessage="This action cannot be undone. Are you sure you want to proceed?"
+            confirmHeading="Confirmation"
+            onConfirm={handleConfirm}
+            style={{ width: 'auto' }}
+        >
+            Delete
+        </ConfirmButton>
+    );
+}
+
+export const Primary: Story = {
+    render: Template,
+};
+
+export const Secondary: Story = {
+    render: Template,
     args: {
-        confirmMessage: 'Are you sure?',
-        confirmHeading: 'Confirmation',
-        children: 'Confirm-button',
+        variant: 'secondary',
+    },
+};
+
+export const Tertiary: Story = {
+    render: Template,
+    args: {
+        variant: 'tertiary',
     },
 };
 
 export const Disabled: Story = {
+    render: Template,
     args: {
-        confirmMessage: 'Are you sure?',
-        confirmHeading: 'Confirmation',
-        children: 'Confirm-button',
         disabled: true,
     },
 };
