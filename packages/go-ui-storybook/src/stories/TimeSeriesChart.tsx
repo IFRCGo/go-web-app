8b192a3c88ecde3a585d76d210fe7b2213167b29
samshara <sameerai2736@gmail.com>
2024-06-05 15:59:01 +0545

    fix: use template story for TimeSeriesChart

---
+++ packages/go-ui-storybook/src/stories/TimeSeriesChart.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react';
+import { TimeSeriesChart as PureTimeSeriesChart } from '@ifrc-go/ui';
+
+// FIXME: let's export TimeSeriesChartProps from @ifrc-go/ui
+type TimeSeriesChartProps<K extends string> = ComponentProps<typeof PureTimeSeriesChart<K>>
+
+function WrappedTimeSeriesChart<const K extends string>(props: TimeSeriesChartProps<K>) {
+    return (
+        <PureTimeSeriesChart {...props} /> // eslint-disable-line react/jsx-props-no-spreading
+    );
+}
+
+export default WrappedTimeSeriesChart;
