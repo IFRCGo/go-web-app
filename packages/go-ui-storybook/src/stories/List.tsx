a90b671ab93ebbd8ba03a33a39fdcb4bd666039a
samshara <sameerai2736@gmail.com>
2024-06-05 17:07:25 +0545

    refactor: List story

--- packages/go-ui-storybook/src/stories/List.tsx
+++ packages/go-ui-storybook/src/stories/List.tsx
@@ -1,18 +1,20 @@
 import {
     List as PureList,
-    ListKey,
     ListProps as PureListProps,
 } from '@ifrc-go/ui';
 
-interface ListProps< DATUM, KEY extends ListKey, RENDERER_PROPS>
-extends PureListProps< DATUM, KEY, RENDERER_PROPS> {}
+type ListKey = string | number | boolean
+interface ListProps<DATUM, KEY extends ListKey, RENDERER_PROPS>
+    extends PureListProps<DATUM, KEY, RENDERER_PROPS> {}
 
-function WrappedList
-// eslint-disable-next-line max-len
-<DATUM, KEY extends ListKey, RENDERER_PROPS>(props: ListProps<DATUM, KEY, RENDERER_PROPS>) {
+function List<
+    DATUM,
+    KEY extends ListKey,
+    RENDERER_PROPS,
+>(props: ListProps<DATUM, KEY, RENDERER_PROPS>) {
     return (
         <PureList {...props} />// eslint-disable-line react/jsx-props-no-spreading
     );
 }
 
-export default WrappedList;
+export default List;
