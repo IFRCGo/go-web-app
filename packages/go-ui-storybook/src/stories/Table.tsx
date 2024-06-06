bd7db6168746ba416db24a3a3fab5cce880d812a
samshara <sameerai2736@gmail.com>
2024-06-06 15:08:51 +0545

    refactor: Table component

--- packages/go-ui-storybook/src/stories/Table.tsx
+++ packages/go-ui-storybook/src/stories/Table.tsx
@@ -5,13 +5,18 @@ import {
     TableProps as PureTableProps,
 } from '@ifrc-go/ui';
 
-// eslint-disable-next-line max-len
-type TableProps<D, K extends string | number, C extends Column<D, K, string, string>> = PureTableProps<D, K, C>;
+type TableProps<
+    D,
+    K extends string | number,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    C extends Column<D, K, any, any>
+> = PureTableProps<D, K, C>;
 
-function WrappedTable<D, K extends string | number, C extends Column<D, K, string, string>>(props: TableProps<D, K, C>) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function Table<D, K extends string | number, C extends Column<D, K, any, any>>(props: TableProps<D, K, C>) {
     return (
         <PureTable {...props} />// eslint-disable-line react/jsx-props-no-spreading
     );
 }
 
-export default WrappedTable;
+export default Table;
