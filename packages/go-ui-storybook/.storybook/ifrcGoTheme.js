1b3574e1b72fe6e210c5da5a677ceb690a1a2049
samshara <sameerai2736@gmail.com>
2024-06-07 10:38:37 +0545

    refactor: Typography story

--- packages/go-ui-storybook/.storybook/ifrcGoTheme.js
+++ packages/go-ui-storybook/.storybook/ifrcGoTheme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
     base: 'light',
     brandTitle: 'IFRC-GO',
-    brandUrl: 'https://go.ifrc.org/',
+    brandUrl: '/',
     brandImage: '/go-logo.svg',
     brandTarget: 'IFRC_GO',
 });
