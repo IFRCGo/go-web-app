3c965a3d0902333d6b6ce8f8beeb5c760dd27d87
roshni73 <roshnipoudel73@gmail.com>
2024-06-07 15:08:11 +0545

    Use Template to write Pager  story fix bugs

--- packages/go-ui-storybook/src/stories/Pager.stories.tsx
+++ packages/go-ui-storybook/src/stories/Pager.stories.tsx
@@ -1,15 +1,15 @@
 import { PagerProps } from '@ifrc-go/ui';
 import { useArgs } from '@storybook/preview-api';
 import type {
+    Args,
     Meta,
     StoryObj,
 } from '@storybook/react';
+import { fn } from '@storybook/test';
 
 import Pager from './Pager';
 
-type PagerSpecificProps = PagerProps & {
-    onChange: (e: number) => void;
-};
+type PagerSpecificProps = PagerProps
 
 type Story = StoryObj<PagerSpecificProps>;
 
@@ -23,52 +23,53 @@ const meta: Meta<typeof Pager> = {
             url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
         },
     },
+    args: {
+        onActivePageChange: fn(),
+    },
     tags: ['autodocs'],
-    decorators: [
-        function Component(_, ctx) {
-            const componentArgs = ctx.args as PagerSpecificProps;
-            const [{ activePage, maxItemsPerPage }, handleArgsChange] = useArgs();
-
-            const setActivePage = (e: number) => {
-                handleArgsChange({ activePage: e });
-            };
-
-            return (
-                <Pager
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    {...componentArgs}
-                    onActivePageChange={setActivePage}
-                    maxItemsPerPage={maxItemsPerPage}
-                    activePage={activePage}
-                />
-            );
-        },
-    ],
 };
 
 export default meta;
 
+function Template(args:Args) {
+    const [{ activePage, maxItemsPerPage }, handleArgsChange] = useArgs();
+
+    const onChange = (e: number) => {
+        handleArgsChange({ activePage: e });
+    };
+    return (
+        <Pager
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...args}
+            onActivePageChange={onChange}
+            maxItemsPerPage={maxItemsPerPage}
+            activePage={activePage}
+            itemsCount={10}
+        />
+    );
+}
+
 export const Default: Story = {
+    render: Template,
     args: {
         activePage: 3,
-        itemsCount: 10,
         maxItemsPerPage: 1,
     },
 };
 
 export const Disabled: Story = {
+    render: Template,
     args: {
         activePage: 5,
-        itemsCount: 10,
         maxItemsPerPage: 1,
         disabled: true,
     },
 };
 
 export const ShowAllPages: Story = {
+    render: Template,
     args: {
         activePage: 5,
-        itemsCount: 10,
         maxItemsPerPage: 1,
         showAllPages: true,
     },
