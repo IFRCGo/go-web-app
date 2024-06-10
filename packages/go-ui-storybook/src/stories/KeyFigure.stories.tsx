c9eefe9151ad32b15ef91eabaedd655849d36931
samshara <sameerai2736@gmail.com>
2024-06-10 20:11:53 +0545

    refactor: KeyFigure component

--- packages/go-ui-storybook/src/stories/KeyFigure.stories.tsx
+++ packages/go-ui-storybook/src/stories/KeyFigure.stories.tsx
@@ -1,4 +1,4 @@
-import { WikiHelpSectionLineIcon } from '@ifrc-go/icons';
+import { HealthadviceIcon } from '@ifrc-go/icons';
 import type {
     Meta,
     StoryObj,
@@ -8,46 +8,46 @@ import KeyFigure from './KeyFigure';
 
 type Story = StoryObj<typeof KeyFigure>
 
-const meta: Meta<typeof KeyFigure> = {
+const meta = {
     title: 'Components/KeyFigure',
     component: KeyFigure,
     parameters: {
         layout: 'centered',
         design: {
             type: 'figma',
-            url: 'https://www.figma.com/file/myeW85ibN5p2SlnXcEpxFD/IFRC-GO---UI-Current---1?type=design&node-id=0-4957&mode=design&t=KwxbuoUQxqcLyZbG-0',
+            url: 'https://www.figma.com/file/k9SOqgh5jk9PxzuBKdMKsA/IFRC-GO---UI-Library?node-id=11176-183007&t=1f3grs60CuPRyWAq-4',
             allowFullscreen: true,
         },
     },
     tags: ['autodocs'],
-    argTypes: {},
-};
+} satisfies Meta<typeof KeyFigure>;
 
 export default meta;
 
-export const Default: Story = {
+export const Default = {
     args: {
         value: 500,
         description: 'Targeted population',
     },
-};
-export const WithProgress: Story = {
+} satisfies Story;
+
+export const WithProgress = {
     args: {
-        value: 500,
-        description: 'Funding Requirements',
+        ...Default.args,
         progress: 50,
         progressDescription: '50% received',
     },
-};
-export const WithIconAndInfo: Story = {
+} satisfies Story;
+
+export const WithIconAndInfo = {
     args: {
-        value: 800,
+        value: 800000,
         label: 'Targeted Population',
         description: 'Funding Requirements',
         progress: 22,
         progressDescription: '22% received',
-        icon: <WikiHelpSectionLineIcon />,
-        info: 'AFRICA - HUNGER CRISIS',
-        children: 'Last updated:2023-10-06',
+        icon: <HealthadviceIcon />,
+        info: 'Africa - Hunger Crisis',
+        children: 'Last updated: 2023-10-06',
     },
-};
+} satisfies Story;
