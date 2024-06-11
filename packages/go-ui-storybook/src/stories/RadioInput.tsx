6f1ec583e83ca2c631300a0741f541040a821ce3
samshara <sameerai2736@gmail.com>
2024-06-11 16:18:42 +0545

    refactor: RadioInput story

--- packages/go-ui-storybook/src/stories/RadioInput.tsx
+++ packages/go-ui-storybook/src/stories/RadioInput.tsx
@@ -5,9 +5,10 @@ import {
 } from '@ifrc-go/ui';
 
 // eslint-disable-next-line max-len
-function WrappedRadioInput<const N, O extends object, V extends string | number | boolean, RRP extends RadioProps<V, N>>(props: PureRadioInputProps<N, O, V, RRP, never>) {
+function RadioInput<const N, O extends object, V extends string | number | boolean, RRP extends RadioProps<V, N>>(props: PureRadioInputProps<N, O, V, RRP, never>) {
     return (
         <PureRadioInput {...props} /> // eslint-disable-line react/jsx-props-no-spreading
     );
 }
-export default WrappedRadioInput;
+
+export default RadioInput;
