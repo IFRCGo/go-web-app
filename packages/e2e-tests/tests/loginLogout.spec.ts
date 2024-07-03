16e325ed950bb706a5475919563cb760ddf5c4ec
babin <babin.karmacharya11@gmail.com>
2024-07-03 17:48:23 +0545

    Use environment variables in playwright tests

--- packages/e2e-tests/tests/loginLogout.spec.ts
+++ packages/e2e-tests/tests/loginLogout.spec.ts
@@ -5,26 +5,26 @@ test("Login", async ({ page }) => {
   await login(
     page,
     process.env.APP_URL,
-    process.env.USER_NAME,
-    process.env.PASSWORD
+    process.env.PLAYWRIGHT_USER_NAME,
+    process.env.PLAYWRIGHT_USER_PASSWORD
   );
   await expect(page.getByRole("navigation")).toContainText(
-    process.env.USER_NAME
+    process.env.PLAYWRIGHT_USER_NAME
   );
-  await page.getByRole("button", { name: process.env.USER_NAME }).click();
+  await page.getByRole("button", { name: process.env.PLAYWRIGHT_USER_NAME }).click();
 });
 
 test("logout", async ({ page }) => {
   await login(
     page,
     process.env.APP_URL,
-    process.env.USER_NAME,
-    process.env.PASSWORD
+    process.env.PLAYWRIGHT_USER_NAME,
+    process.env.PLAYWRIGHT_USER_PASSWORD
   );
   await expect(page.getByRole("navigation")).toContainText(
-    process.env.USER_NAME
+    process.env.PLAYWRIGHT_USER_NAME
   );
-  await page.getByRole("button", { name: process.env.USER_NAME }).click();
+  await page.getByRole("button", { name: process.env.PLAYWRIGHT_USER_NAME }).click();
   await page.getByRole("button", { name: "Logout" }).click();
   await page.getByRole("button", { name: "Ok" }).click();
   await expect(page.getByRole("navigation")).toContainText("Login");
