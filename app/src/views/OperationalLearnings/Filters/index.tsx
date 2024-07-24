a2a77d4ea474c72506e6c46ba3843063d1053363
roshni73 <roshani.poudel@togglecorp.com>
2024-07-24 16:48:05 +0545

    Add clear filter and selected filter view.

--- app/src/views/OperationalLearnings/Filters/index.tsx
+++ app/src/views/OperationalLearnings/Filters/index.tsx
@@ -1,20 +1,27 @@
 import { useCallback } from 'react';
+import { SearchLineIcon } from '@ifrc-go/icons';
 import {
+    Button,
     Container,
     DateInput,
     SelectInput,
     TextInput,
 } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
+import { isDefined } from '@togglecorp/fujs';
 
 import CountrySelectInput from '#components/domain/CountrySelectInput';
 import DisasterTypeSelectInput from '#components/domain/DisasterTypeSelectInput';
 import ExportButton from '#components/domain/ExportButton';
 import PrimarySectorSelectInput from '#components/domain/PrimarySectorSelectInput';
 import RegionSelectInput, { RegionOption } from '#components/domain/RegionSelectInput';
+import useCountry from '#hooks/domain/useCountry';
+import useDisasterTypes from '#hooks/domain/useDisasterType';
+import useRegion from '#hooks/domain/useRegion';
 import useFilterState from '#hooks/useFilterState';
 
 import i18n from './i18n.json';
+import styles from './styles.module.css';
 
 type Option = {
     key: string;
@@ -29,11 +36,13 @@ function Filters() {
 
     const {
         rawFilter,
+        resetFilter,
         setFilter,
         setFilterField,
+        filtered,
     } = useFilterState<{
-        region?: RegionOption['key'],
-        country?: number,
+        region?:RegionOption['key'],
+        country?:number,
         disasterType?:number,
         startDateBefore?: string,
         bySector?:number,
@@ -45,6 +54,33 @@ function Filters() {
     });
 
     const handleExportClick = useCallback(() => setFilter({}), [setFilter]);
+    const disasterTypes = useDisasterTypes();
+    const disasterTypeName = (
+        id: number | undefined,
+    ): string | undefined => disasterTypes?.find((type) => type.id === id)?.name;
+
+    const selectedFilters = [
+        {
+            value: rawFilter.region,
+            label: useRegion({ id: rawFilter.region })?.region_name,
+        },
+        {
+            value: rawFilter.country,
+            label: useCountry({ id: rawFilter.country })?.name,
+        },
+        {
+            value: rawFilter.disasterType,
+            label: disasterTypeName(rawFilter.disasterType),
+        },
+        {
+            value: rawFilter.bySector,
+            label: rawFilter.bySector,
+        },
+        {
+            value: rawFilter.byComponent,
+            label: rawFilter.byComponent,
+        },
+    ].filter((filter) => isDefined(filter.value) && isDefined(filter.label));
 
     return (
         <Container
@@ -55,8 +91,8 @@ function Filters() {
                     <RegionSelectInput
                         placeholder={strings.filterRegionsPlaceholder}
                         name="region"
-                        onChange={setFilterField}
                         value={rawFilter.region}
+                        onChange={setFilterField}
                     />
                     <CountrySelectInput
                         placeholder={strings.filterCountryPlaceholder}
@@ -98,7 +134,18 @@ function Filters() {
                         placeholder={strings.filterOpsLearningsSearchPlaceholder}
                         value={rawFilter.search}
                         onChange={setFilterField}
+                        icons={<SearchLineIcon />}
                     />
+                    <div className={styles.actions}>
+                        <Button
+                            name={undefined}
+                            variant="secondary"
+                            onClick={resetFilter}
+                            disabled={!filtered}
+                        >
+                            {strings.opsLearningsFilterClear}
+                        </Button>
+                    </div>
                 </>
             )}
             actions={(
@@ -109,7 +156,18 @@ function Filters() {
                     totalCount={0}
                 />
             )}
-        />
+        >
+            {selectedFilters.length > 0 && (
+                <div className={styles.selectedFilters}>
+                    {strings.opsLearningsSelectedFilterView}
+                    {selectedFilters.map((filter) => (
+                        <div key={filter.value}>
+                            {filter.label}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </Container>
     );
 }
 
