3e325e79bda9a239ba31a88fe98e43869656811e
roshni73 <roshani.poudel@togglecorp.com>
2024-08-08 16:55:37 +0545

    Fetch summaries and key insights from API

--- app/src/views/OperationalLearnings/BySector/index.tsx
+++ app/src/views/OperationalLearnings/BySector/index.tsx
@@ -1,25 +1,45 @@
 import { Container } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
+import { resolveToString } from '@ifrc-go/ui/utils';
+
+import { useRequest } from '#utils/restRequest';
 
 import i18n from './i18n.json';
 
 function BySector() {
     const strings = useTranslation(i18n);
+    const {
+        response: summariesResponse,
+    } = useRequest({
+        url: '/api/v2/ops-learning/summary/',
+        preserveResponse: true,
+    });
+
     return (
         <Container
-            heading="health"
-            headerDescription={strings.opsLearningsSummariesDescription}
-            actions={(
-                <div>
-                    66 source 13 operations
-                </div>
-            )}
-            footerContent={(
-                <div>
-                    see sources
-                </div>
-            )}
-        />
+            headingLevel={2}
+            spacing="relaxed"
+        >
+            { summariesResponse?.sectors.map((result) => (
+                <Container
+                    heading={result.title}
+                    headerDescription={result.content}
+                    headingDescription={resolveToString(
+                        strings.bySectorsExtracts,
+                        { count: summariesResponse.components.length },
+                    )}
+                    actions={resolveToString(
+                        strings.bySectorSourceOperations,
+                        { count: summariesResponse.components.length },
+                    )}
+                    footerActions={(
+                        <div>
+                            {strings.bySectorSeeSource}
+                        </div>
+                    )}
+                />
+            ))}
+        </Container>
     );
 }
 
