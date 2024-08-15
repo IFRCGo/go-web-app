fbb9d9bc9c807e3383b028abdbeaada47b28c06e
samshara <sameerai2736@gmail.com>
2024-08-15 17:23:43 +0545

    feat: integrate operational learning summary and extracts API

---
+++ app/src/views/OperationalLearnings/Summary/index.tsx
@@ -0,0 +1,71 @@
+import {
+    ArrowDownSmallFillIcon,
+    ArrowUpSmallFillIcon,
+} from '@ifrc-go/icons';
+import {
+    Button,
+    Container,
+} from '@ifrc-go/ui';
+import {
+    useBooleanState,
+    useTranslation,
+} from '@ifrc-go/ui/hooks';
+import { resolveToString } from '@ifrc-go/ui/utils';
+
+import Sources from '../Sources';
+
+import i18n from './i18n.json';
+
+export interface Props {
+    id: number;
+    summaryType: 'sector' | 'component';
+    summaryTitle: string;
+    extractsCount: number;
+    summaryContent: string | null | undefined;
+}
+
+function Summary(props: Props) {
+    const {
+        id,
+        summaryType,
+        summaryTitle,
+        extractsCount,
+        summaryContent,
+    } = props;
+
+    const strings = useTranslation(i18n);
+
+    const [
+        isExpanded,
+        {
+            toggle: toggleExpansion,
+        },
+    ] = useBooleanState(false);
+
+    return (
+        <Container
+            heading={summaryTitle}
+            headingDescription={resolveToString(strings.extractsCount, { count: extractsCount })}
+            footerActions={(
+                <Button
+                    name={id}
+                    variant="tertiary"
+                    onClick={toggleExpansion}
+                    actions={(isExpanded
+                        ? <ArrowUpSmallFillIcon />
+                        : <ArrowDownSmallFillIcon />
+                    )}
+                >
+                    {isExpanded ? strings.closeSources : strings.seeSources}
+                </Button>
+            )}
+            footerContent={isExpanded && (
+                <Sources summaryId={id} summaryType={summaryType} />
+            )}
+        >
+            {summaryContent}
+        </Container>
+    );
+}
+
+export default Summary;
