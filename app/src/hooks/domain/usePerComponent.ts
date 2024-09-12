9f26a262c012ca73188b3b81d8a8a5960938d536
samshara <sameerai2736@gmail.com>
2024-09-12 14:07:50 +0545

    fix: fetch and cache secondary sectors and components in domain context

--- app/src/hooks/domain/usePerComponent.ts
+++ app/src/hooks/domain/usePerComponent.ts
@@ -1,10 +1,13 @@
-import { useMemo } from 'react';
+import {
+    useContext,
+    useEffect,
+    useMemo,
+} from 'react';
 import { isDefined } from '@togglecorp/fujs';
 
-import { useRequest } from '#utils/restRequest';
-import { type GoApiResponse } from '#utils/restRequest';
+import DomainContext, { PerComponents } from '#contexts/domain';
 
-export type PerComponent = NonNullable<GoApiResponse<'/api/v2/per-formcomponent/'>['results']>[number];
+export type PerComponent = NonNullable<PerComponents['results']>[number];
 
 type ListProps = {
     id?: never;
@@ -18,32 +21,37 @@ function usePerComponent(props?: ListProps): [Array<PerComponent> | undefined, b
 function usePerComponent(props: PropsForId): [PerComponent | undefined, boolean]
 function usePerComponent(
     props?: ListProps | PropsForId,
-): [PerComponent | undefined | Array<PerComponent> | undefined, boolean] {
+): [PerComponent | undefined | Array<PerComponent> | undefined, boolean | undefined] {
     const {
-        pending: perFormComponentResponsePending,
-        response: perFormComponentResponse,
-    } = useRequest({
-        url: '/api/v2/per-formcomponent/',
-        preserveResponse: true,
-    });
+        register,
+        perComponents,
+        perComponentsPending,
+    } = useContext(DomainContext);
+
+    useEffect(
+        () => {
+            register('per-components');
+        },
+        [register],
+    );
 
     const returnValue = useMemo(
         () => {
             const id = props?.id;
             if (isDefined(id)) {
-                return perFormComponentResponse
+                return perComponents
                     ?.results?.find((perComponent) => perComponent.id === id);
             }
 
             // NOTE: we need to filter out parent components
-            return perFormComponentResponse?.results?.filter(
+            return perComponents?.results?.filter(
                 (perComponent) => !perComponent.is_parent,
             );
         },
-        [perFormComponentResponse, props?.id],
+        [perComponents, props?.id],
     );
 
-    return [returnValue, perFormComponentResponsePending];
+    return [returnValue, perComponentsPending];
 }
 
 export default usePerComponent;
