e7932c3d4e04a946b9fba683fe724d6a31848731
samshara <sameerai2736@gmail.com>
2025-01-23 17:51:41 +0545

    feat(operational-learning): show source details on click for sources over time map

--- app/src/views/OperationalLearning/Stats/index.tsx
+++ app/src/views/OperationalLearning/Stats/index.tsx
@@ -9,10 +9,15 @@ import {
     Container,
     KeyFigure,
     LegendItem,
+    TextOutput,
     TimeSeriesChart,
 } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
-import { getDatesSeparatedByYear } from '@ifrc-go/ui/utils';
+import {
+    formatDate,
+    getDatesSeparatedByYear,
+    getFormattedDateKey,
+} from '@ifrc-go/ui/utils';
 import {
     isDefined,
     isNotDefined,
@@ -46,6 +51,12 @@ const regionValueSelector = (datum: RegionStatItem) => datum.count;
 const regionLabelSelector = (datum: RegionStatItem) => datum.region_name;
 
 type SourceType = 'dref' | 'emergencyAppeal' | 'others';
+interface SourceTypeOption {
+    key: SourceType;
+    label: string;
+    color: string;
+}
+
 const dataKeys: SourceType[] = [
     'dref',
     'emergencyAppeal',
@@ -62,11 +73,6 @@ const xAxisFormatter = (date: Date) => date.toLocaleString(
     { year: 'numeric' },
 );
 
-interface SourceTypeOption {
-    key: SourceType;
-    label: string;
-    color: string;
-}
 type SourceTypeEnum = components<'read'>['schemas']['ApiAppealTypeEnumKey'];
 const SOURCE_TYPE_EMERGENCY = 1 satisfies SourceTypeEnum;
 const SOURCE_TYPE_DREF = 0 satisfies SourceTypeEnum;
@@ -75,13 +81,13 @@ const transformSourcesOverTimeData = (data: SourcesOverTimeItem[]) => {
     const groupedData: Record<string, Record<SourceType, number>> = {};
 
     data.forEach((entry) => {
-        const year = new Date(entry.date).getFullYear().toString();
+        const year = getFormattedDateKey(entry.date);
         if (!groupedData[year]) {
             groupedData[year] = { dref: 0, emergencyAppeal: 0, others: 0 };
         }
-        if (entry.type === SOURCE_TYPE_DREF) {
+        if (entry.atype === SOURCE_TYPE_DREF) {
             groupedData[year].dref += entry.count;
-        } else if (entry.type === SOURCE_TYPE_EMERGENCY) {
+        } else if (entry.atype === SOURCE_TYPE_EMERGENCY) {
             groupedData[year].emergencyAppeal += entry.count;
         } else {
             groupedData[year].others += entry.count;
@@ -127,11 +133,12 @@ function Stats(props: Props) {
         },
         [learningStatsResponse],
     );
+
     const dateList = useMemo(() => {
         if (isNotDefined(sourcesOverTimeData)) {
             return undefined;
         }
-        const dates = Object.keys(sourcesOverTimeData).map((year) => new Date(Number(year), 0, 1));
+        const dates = Object.keys(sourcesOverTimeData).map((year) => new Date(year));
         const oldestDate = new Date(Math.min(...dates.map((date) => date.getTime())));
         const latestDate = new Date(Math.max(...dates.map((date) => date.getTime())));
         return getDatesSeparatedByYear(oldestDate, latestDate);
@@ -139,7 +146,7 @@ function Stats(props: Props) {
 
     const sourcesOverTimeValueSelector = useCallback(
         (key: SourceType, date: Date) => {
-            const value = sourcesOverTimeData?.[date.getFullYear()]?.[key];
+            const value = sourcesOverTimeData?.[getFormattedDateKey(date)]?.[key];
             if (isDefined(value) && value > 0) {
                 return value;
             }
@@ -170,26 +177,32 @@ function Stats(props: Props) {
         strings.sourceOthers,
     ]);
 
+    const activePointData = activePointKey ? sourcesOverTimeData?.[activePointKey] : undefined;
+
     return (
         <div className={styles.stats}>
             {learningStatsPending && <BlockLoading />}
-            <div className={styles.keyFigureCard}>
+            <div className={styles.keyFigureList}>
                 <KeyFigure
+                    className={styles.keyFigure}
                     value={learningStatsResponse?.operations_included}
                     label={strings.operationsIncluded}
                     labelClassName={styles.keyFigureDescription}
                 />
                 <KeyFigure
+                    className={styles.keyFigure}
                     value={learningStatsResponse?.sources_used}
                     label={strings.sourcesUsed}
                     labelClassName={styles.keyFigureDescription}
                 />
                 <KeyFigure
+                    className={styles.keyFigure}
                     value={learningStatsResponse?.learning_extracts}
                     label={strings.learningExtract}
                     labelClassName={styles.keyFigureDescription}
                 />
                 <KeyFigure
+                    className={styles.keyFigure}
                     value={learningStatsResponse?.sectors_covered}
                     label={strings.sectorsCovered}
                     labelClassName={styles.keyFigureDescription}
@@ -245,38 +258,84 @@ function Stats(props: Props) {
                         withInternalPadding
                         compactMessage
                         pending={learningStatsPending}
+                        childrenContainerClassName={styles.chartContainer}
                         empty={isDefined(learningStatsResponse?.sources_overtime) && (
                             (learningStatsResponse?.sources_overtime.length ?? 0) < 1
                         )}
-                        footerIcons={(
-                            <div className={styles.typeOfSourceLegend}>
-                                <div className={styles.legendLabel}>
-                                    {strings.sourcesTypeLegendLabel}
-                                </div>
-                                <div className={styles.legendContent}>
-                                    {sourceTypeOptions.map((source) => (
-                                        <LegendItem
-                                            key={source.key}
-                                            label={source.label}
-                                            color={source.color}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                        )}
                     >
                         {isDefined(dateList) && (
-                            <TimeSeriesChart
-                                className={styles.timeSeriesChart}
-                                xAxisTickClassName={styles.xAxisTick}
-                                timePoints={dateList}
-                                dataKeys={dataKeys}
-                                valueSelector={sourcesOverTimeValueSelector}
-                                classNameSelector={sourceClassNameSelector}
-                                activePointKey={activePointKey}
-                                onTimePointClick={setActivePointKey}
-                                xAxisFormatter={xAxisFormatter}
-                            />
+                            <>
+                                <TimeSeriesChart
+                                    className={styles.timeSeriesChart}
+                                    xAxisTickClassName={styles.xAxisTick}
+                                    timePoints={dateList}
+                                    dataKeys={dataKeys}
+                                    valueSelector={sourcesOverTimeValueSelector}
+                                    classNameSelector={sourceClassNameSelector}
+                                    activePointKey={activePointKey}
+                                    onTimePointClick={setActivePointKey}
+                                    xAxisFormatter={xAxisFormatter}
+                                />
+                                {isDefined(activePointKey) ? (
+                                    <div
+                                        className={styles.legend}
+                                    >
+                                        <TextOutput
+                                            value={formatDate(activePointKey, 'yyyy') ?? '--'}
+                                            strongValue
+                                        />
+                                        <TextOutput
+                                            label={(
+                                                <LegendItem
+                                                    label={strings.sourceDREF}
+                                                    color="var(--color-source-dref)"
+                                                />
+                                            )}
+                                            withoutLabelColon
+                                            value={activePointData?.dref}
+                                            valueType="number"
+                                        />
+                                        <TextOutput
+                                            label={(
+                                                <LegendItem
+                                                    label={strings.sourceEmergencyAppeal}
+                                                    color="var(--color-source-emergency-appeal)"
+                                                />
+                                            )}
+                                            withoutLabelColon
+                                            value={activePointData?.emergencyAppeal}
+                                            valueType="number"
+                                        />
+                                        <TextOutput
+                                            label={(
+                                                <LegendItem
+                                                    label={strings.sourceOthers}
+                                                    color="var(--color-source-emergency-appeal)"
+                                                />
+                                            )}
+                                            withoutLabelColon
+                                            value={activePointData?.others}
+                                            valueType="number"
+                                        />
+                                    </div>
+                                ) : (
+                                    <div className={styles.typeOfSourceLegend}>
+                                        <div className={styles.legendLabel}>
+                                            {strings.sourcesTypeLegendLabel}
+                                        </div>
+                                        <div className={styles.legendContent}>
+                                            {sourceTypeOptions.map((source) => (
+                                                <LegendItem
+                                                    key={source.key}
+                                                    label={source.label}
+                                                    color={source.color}
+                                                />
+                                            ))}
+                                        </div>
+                                    </div>
+                                )}
+                            </>
+
                         )}
 
                     </Container>
