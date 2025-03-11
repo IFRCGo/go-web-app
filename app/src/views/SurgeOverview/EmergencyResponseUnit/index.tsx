5439179d1d341066efe41483bbf463c8e270a974
barshathakuri <barshathakuri708@gmail.com>
2025-03-11 17:52:27 +0545

    Add Update ERU Form

--- app/src/views/SurgeOverview/EmergencyResponseUnit/index.tsx
+++ app/src/views/SurgeOverview/EmergencyResponseUnit/index.tsx
@@ -1,12 +1,21 @@
 import Link from '#components/Link';
 
+export interface Props {
+    eruId: number;
+}
+
 /** @knipignore */
 // eslint-disable-next-line import/prefer-default-export
-export function Component() {
+export function Component(props: Props) {
+    const {
+        eruId,
+    } = props;
+
     return (
         <Link
             to="updateERUReadinessForm"
             variant="primary"
+            state={{ eruId }}
         >
             Update ERU Readiness
         </Link>
