cdc6ce40c22174cbd6eb686a51bf5d37e0186cbc
Szabo Zoltan <szabozoltan69@gmail.com>
2025-09-25 13:26:56 +0200

    Add HR technical competency framework box

--- .changeset/fluffy-lions-nail.md
+++ .changeset/fluffy-lions-nail.md
@@ -3,6 +3,7 @@
 ---
 
 Surge updates
+  - Fix the "Catalogue of Surge Services" tab highlight issue
   - CoS – Logistics ERU: Minor textual changes on /surge/catalogue/logistics/eru
   - Operational Timeline Toolbox update (email replacement)
-  - Fix the "Catalogue of Surge Services" tab highlight issue
+  - Add HR technical competency framework box (and PDF)
--- app/src/views/SurgeCatalogueOtherHumanResources/index.tsx
+++ app/src/views/SurgeCatalogueOtherHumanResources/index.tsx
@@ -1,8 +1,10 @@
+import { useMemo } from 'react';
 import { useTranslation } from '@ifrc-go/ui/hooks';
 
+import CatalogueInfoCard, { type LinkData } from '#components/CatalogueInfoCard';
+import SurgeCardContainer from '#components/domain/SurgeCardContainer';
 import SurgeCatalogueContainer from '#components/domain/SurgeCatalogueContainer';
 import SurgeContentContainer from '#components/domain/SurgeContentContainer';
-import Link from '#components/Link';
 
 import i18n from './i18n.json';
 
@@ -11,40 +13,52 @@ import i18n from './i18n.json';
 export function Component() {
     const strings = useTranslation(i18n);
 
+    const roleProfiles = useMemo<LinkData[]>(() => [
+        {
+            title: strings.hrLinkOneCoord,
+            href: 'https://ifrcorg.sharepoint.com/:b:/s/IFRCSharing/EQ2XIVDyiHFGh8G27WHDgnEBxS_tdNbtzfFw6nV6HXRaag',
+            external: true,
+            withLinkIcon: true,
+        },
+        {
+            title: strings.hrLinkTwoOfficer,
+            href: 'https://ifrcorg.sharepoint.com/:b:/s/IFRCSharing/ETtWcMNEWz9LoNsUSQQSyiYBPTLLuZyur7y74Ho8l0ctxA',
+            external: true,
+            withLinkIcon: true,
+        },
+    ], [strings]);
+
+    const frameworkData = useMemo<LinkData[]>(() => [
+        {
+            title: strings.hrCategoriesCompetencies,
+            href: 'https://ifrcorg.sharepoint.com/:b:/s/IFRCSharing/EVfz7uqjMHtFjE1JD8bd8TEBoWP_f-qIjBRQ-pkwKKDZkQ',
+            external: true,
+            withLinkIcon: true,
+        },
+    ], [strings]);
+
     return (
         <SurgeCatalogueContainer
-            heading={strings.humanHeading}
+            heading={strings.hrHeading}
         >
             <SurgeContentContainer
-                heading={strings.otherOverview}
+                heading={strings.hrOverview}
             >
-                <div>{strings.otherOverviewDetail}</div>
+                <div>{strings.hrOverviewDetail}</div>
             </SurgeContentContainer>
-            <SurgeContentContainer
+            <SurgeCardContainer
                 heading={strings.rapidResponse}
             >
-                <div>{strings.rapidResponseDetail}</div>
-                <ul>
-                    <li>
-                        <Link
-                            href="https://ifrcorg.sharepoint.com/:b:/s/IFRCSharing/EQ2XIVDyiHFGh8G27WHDgnEBxS_tdNbtzfFw6nV6HXRaag"
-                            external
-                            withLinkIcon
-                        >
-                            {strings.otherLinkOneCoord}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href="https://ifrcorg.sharepoint.com/:b:/s/IFRCSharing/ETtWcMNEWz9LoNsUSQQSyiYBPTLLuZyur7y74Ho8l0ctxA"
-                            external
-                            withLinkIcon
-                        >
-                            {strings.otherLinkTwoOffice}
-                        </Link>
-                    </li>
-                </ul>
-            </SurgeContentContainer>
+                <CatalogueInfoCard
+                    title={strings.roleProfiles}
+                    data={roleProfiles}
+                />
+                <CatalogueInfoCard
+                    title={strings.hrTechnical}
+                    data={frameworkData}
+                    description={strings.hrTechnicalDescription}
+                />
+            </SurgeCardContainer>
         </SurgeCatalogueContainer>
     );
 }
