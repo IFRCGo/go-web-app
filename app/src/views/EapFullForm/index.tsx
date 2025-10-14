5be1d4e2eca711ee43f119af55c6465c5cbc5a9b
barshathakuri <barshathakuri708@gmail.com>
2025-10-14 13:29:53 +0545

    eap(add-eap-link): Add EAP Tab in account page

---
+++ app/src/views/EapFullForm/index.tsx
@@ -0,0 +1,12 @@
+import Page from '#components/Page';
+
+/** @knipignore */
+// eslint-disable-next-line import/prefer-default-export
+export function Component() {
+    return (
+        <Page>
+            {/* TODO: Add EAP form */}
+            Full EAP Form
+        </Page>
+    );
+}
