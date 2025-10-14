a892d57e002df737bd9a7908d41094d7ef795377
barshathakuri <barshathakuri708@gmail.com>
2025-10-14 13:29:53 +0545

    eap(add-eap-link): Add EAP Tab in account page

---
+++ app/src/views/SimplifiedEapForm/index.tsx
@@ -0,0 +1,12 @@
+import Page from '#components/Page';
+
+/** @knipignore */
+// eslint-disable-next-line import/prefer-default-export
+export function Component() {
+    return (
+        <Page>
+            {/* TODO: Add Simplified EAP form */}
+            Simplified EAP form
+        </Page>
+    );
+}
