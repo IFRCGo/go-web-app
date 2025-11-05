c6edf32a67a615b5609906490168f13c91cce307
barshathakuri <barshathakuri708@gmail.com>
2025-11-05 11:34:02 +0545

    dref-process(tabs): Add tabs in dref process link

--- app/src/views/EarlyActionProtocols/index.tsx
+++ app/src/views/EarlyActionProtocols/index.tsx
@@ -5,7 +5,6 @@ import {
 import { useTranslation } from '@ifrc-go/ui/hooks';
 
 import Link from '#components/Link';
-import Page from '#components/Page';
 
 import i18n from './i18n.json';
 import styles from './styles.module.css';
@@ -16,51 +15,49 @@ export function Component() {
     const strings = useTranslation(i18n);
 
     return (
-        <Page
-            heading={strings.eapHeading}
-            description={strings.eapDescription}
-        >
-            <Container
-                heading={strings.eapTitle}
-                actions={(
-                    <Link
-                        to="eapDevelopmentRegistration"
-                        variant="primary"
-                    >
-                        {strings.eapRegistrationLink}
-                    </Link>
-                )}
-                childrenContainerClassName={styles.earlyActionProtocols}
-            >
-                <ExpandableContainer
-                    heading={strings.eapContent}
+        <Container
+            className={styles.eapDetail}
+            contentViewType="vertical"
+            childrenContainerClassName={styles.content}
+            actions={(
+                <Link
+                    to="eapDevelopmentRegistration"
+                    variant="secondary"
                 >
-                    <p>
-                        {strings.eapContentHeading}
-                    </p>
-                    <p>
-                        {strings.eapContentSubHeadingOne}
-                        <ul>
-                            <li>
-                                {strings.eapDescriptionOne}
-                            </li>
-                            <li>
-                                {strings.eapDescriptionTwo}
-                            </li>
-                            <li>
-                                {strings.eapDescriptionThree}
-                            </li>
-                        </ul>
-                    </p>
-                </ExpandableContainer>
-                {/* TODO: Add remaining content */}
-                <ExpandableContainer
-                    heading={strings.eapContentSubHeadingTwo}
-                />
-                <ExpandableContainer
-                    heading={strings.eapContentSubHeadingThree}
-                />
-            </Container>
-        </Page>
+                    {strings.eapRegistrationLink}
+                </Link>
+            )}
+        >
+            <ExpandableContainer
+                heading={strings.eapContent}
+            >
+                <p>
+                    {strings.eapContentHeading}
+                </p>
+                <p>
+                    {strings.eapContentSubHeadingOne}
+                    <ul>
+                        <li>
+                            {strings.eapDescriptionOne}
+                        </li>
+                        <li>
+                            {strings.eapDescriptionTwo}
+                        </li>
+                        <li>
+                            {strings.eapDescriptionThree}
+                        </li>
+                    </ul>
+                </p>
+            </ExpandableContainer>
+            {/* TODO: Add remaining content */}
+            <ExpandableContainer
+                heading={strings.eapContentSubHeadingTwo}
+            />
+            <ExpandableContainer
+                heading={strings.eapContentSubHeadingThree}
+            />
+        </Container>
     );
 }
+
+Component.displayName = 'EarlyActionProtocols';
