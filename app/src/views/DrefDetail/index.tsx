f62499e10bd0de7087045ad0ba178ee59e8e12e7
barshathakuri <barshathakuri708@gmail.com>
2025-11-05 11:34:02 +0545

    dref-process(tabs): Add tabs in dref process link

---
+++ app/src/views/DrefDetail/index.tsx
@@ -0,0 +1,56 @@
+import { Container } from '@ifrc-go/ui';
+import { useTranslation } from '@ifrc-go/ui/hooks';
+
+import SurgeContentContainer from '#components/domain/SurgeContentContainer';
+import Link from '#components/Link';
+
+import i18n from './i18n.json';
+import styles from './styles.module.css';
+
+/** @knipignore */
+// eslint-disable-next-line import/prefer-default-export
+export function Component() {
+    const strings = useTranslation(i18n);
+
+    return (
+        <Container
+            className={styles.drefDetail}
+            contentViewType="vertical"
+            childrenContainerClassName={styles.content}
+        >
+            <SurgeContentContainer
+                heading={strings.drefIntroHeading}
+            >
+                <div>{strings.drefIntroDetailOne}</div>
+                <div>{strings.drefIntroDetailTwo}</div>
+            </SurgeContentContainer>
+            <SurgeContentContainer
+                heading={strings.drefProcessHeading}
+            >
+                <div>{strings.drefProcessSubHeading}</div>
+                <ul>
+                    <li>
+                        {strings.drefProcessListOne}
+                    </li>
+                    <li>
+                        {strings.drefProcessListTwo}
+                    </li>
+                </ul>
+                <div>
+                    {strings.drefProcessDetailOne}
+                </div>
+                <div>
+                    {strings.drefProcessDetailTwo}
+                </div>
+            </SurgeContentContainer>
+            <Link
+                to="newDrefApplicationForm"
+                withUnderline
+            >
+                {strings.drefDrefApplication}
+            </Link>
+        </Container>
+    );
+}
+
+Component.displayName = 'DrefDetail';
