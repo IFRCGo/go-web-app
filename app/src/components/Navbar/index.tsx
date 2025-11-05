7a379378191bea24a1026f293d20b18fcfafa4ba
barshathakuri <barshathakuri708@gmail.com>
2025-11-05 11:34:02 +0545

    dref-process(tabs): Add tabs in dref process link

--- app/src/components/Navbar/index.tsx
+++ app/src/components/Navbar/index.tsx
@@ -367,11 +367,19 @@ function Navbar(props: Props) {
                                         </Description>
                                         <DropdownMenuItem
                                             type="link"
-                                            to="accountMyFormsDref"
+                                            to="drefDetail"
                                             styleVariant="action"
                                             withoutFullWidth
                                         >
                                             {strings.myDrefApplications}
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem
+                                            type="link"
+                                            to="eapDetail"
+                                            styleVariant="action"
+                                            withoutFullWidth
+                                        >
+                                            {strings.earlyActionProtocols}
                                         </DropdownMenuItem>
                                         <DropdownMenuItem
                                             type="link"
