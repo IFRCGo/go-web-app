41229e81aeeec6392e05e0cfdc521f8f29c9bb0d
barshathakuri <barshathakuri708@gmail.com>
2025-11-10 16:54:31 +0545

    eap-table(table): Add eap development table

---
+++ app/src/views/EapApplications/EapTableActions/index.tsx
@@ -0,0 +1,48 @@
+import { TableActions } from '@ifrc-go/ui';
+import { useTranslation } from '@ifrc-go/ui/hooks';
+
+import DropdownMenuItem from '#components/DropdownMenuItem';
+import Link from '#components/Link';
+
+import i18n from './i18n.json';
+
+export interface Props {
+    eapId: number;
+}
+
+function EapTableActions(props: Props) {
+    const { eapId } = props;
+
+    const strings = useTranslation(i18n);
+
+    return (
+        <TableActions
+            extraActions={(
+                <DropdownMenuItem
+                    type="link"
+                    to="eapDevelopmentRegistrationForm"
+                    urlParams={{ eapId }}
+                >
+                    {strings.eapViewLabel}
+                </DropdownMenuItem>
+            )}
+        >
+            <Link
+                to="eapFullForm"
+                urlParams={{ eapId }}
+                variant="secondary"
+            >
+                {strings.eapFormLink}
+            </Link>
+            <Link
+                to="simplifiedEapForm"
+                urlParams={{ eapId }}
+                variant="secondary"
+            >
+                {strings.simplifiedEapLink}
+            </Link>
+        </TableActions>
+    );
+}
+
+export default EapTableActions;
