5cb026c97cb5d756abc77f3a7934e88c7abe4786
Shreeyash Shrestha <shreeyash.shrestha@togglecorp.com>
2025-11-18 11:56:11 +0545

    fix(eap): update eap section according to new UI

--- app/src/views/EarlyActionProtocols/index.tsx
+++ app/src/views/EarlyActionProtocols/index.tsx
@@ -1,13 +1,13 @@
 import {
     Container,
     ExpandableContainer,
+    ListView,
 } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
 
 import Link from '#components/Link';
 
 import i18n from './i18n.json';
-import styles from './styles.module.css';
 
 /** @knipignore */
 // eslint-disable-next-line import/prefer-default-export
@@ -16,46 +16,54 @@ export function Component() {
 
     return (
         <Container
-            className={styles.eapDetail}
-            contentViewType="vertical"
-            childrenContainerClassName={styles.content}
-            actions={(
+            headerActions={(
                 <Link
                     to="newEapDevelopmentRegistration"
-                    variant="secondary"
+                    styleVariant="outline"
+                    colorVariant="primary"
                 >
                     {strings.eapRegistrationLink}
                 </Link>
             )}
         >
-            <ExpandableContainer
-                heading={strings.eapContent}
+            <ListView
+                layout="block"
             >
-                <p>
-                    {strings.eapContentHeading}
-                </p>
-                <p>
-                    {strings.eapContentSubHeadingOne}
-                    <ul>
-                        <li>
-                            {strings.eapDescriptionOne}
-                        </li>
-                        <li>
-                            {strings.eapDescriptionTwo}
-                        </li>
-                        <li>
-                            {strings.eapDescriptionThree}
-                        </li>
-                    </ul>
-                </p>
-            </ExpandableContainer>
-            {/* TODO: Add remaining content */}
-            <ExpandableContainer
-                heading={strings.eapContentSubHeadingTwo}
-            />
-            <ExpandableContainer
-                heading={strings.eapContentSubHeadingThree}
-            />
+                <ExpandableContainer
+                    heading={strings.eapContent}
+                >
+                    <p>
+                        {strings.eapContentHeading}
+                    </p>
+                    <p>
+                        {strings.eapContentSubHeadingOne}
+                        <ul>
+                            <li>
+                                {strings.eapDescriptionOne}
+                            </li>
+                            <li>
+                                {strings.eapDescriptionTwo}
+                            </li>
+                            <li>
+                                {strings.eapDescriptionThree}
+                            </li>
+                        </ul>
+                    </p>
+                </ExpandableContainer>
+                {/* TODO: Add remaining content */}
+                <ExpandableContainer
+                    heading={strings.eapContentSubHeadingTwo}
+                >
+                    {/* TODO: Add real content and replace with strings */}
+                    EAP content sub heading description two
+                </ExpandableContainer>
+                <ExpandableContainer
+                    heading={strings.eapContentSubHeadingThree}
+                >
+                    {/* TODO: Add real content and replace with strings */}
+                    EAP content sub heading description three
+                </ExpandableContainer>
+            </ListView>
         </Container>
     );
 }
