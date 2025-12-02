72a2645aafeaedfc2d535bed51e1463e9684d9a2
frozenhelium <ankit.mehta@togglecorp.com>
2025-12-02 12:34:33 +0545

    feat(eap): add export for simplified EAP

--- packages/ui/src/hooks/index.tsx
+++ packages/ui/src/hooks/index.tsx
@@ -5,4 +5,5 @@ export { default as useDebouncedValue } from './useDebouncedValue';
 export { default as useFloatPlacement } from './useFloatPlacement';
 export { default as useKeyboard } from './useKeyboard';
 export { default as useSizeTracking } from './useSizeTracking';
+export { default as useSpacingToken } from './useSpacingToken';
 export { default as useTranslation } from './useTranslation';
