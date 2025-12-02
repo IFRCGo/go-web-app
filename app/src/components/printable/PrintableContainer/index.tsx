f2e555cfeb4376b37d6aaf95be8971fefa03d2b7
frozenhelium <ankit.mehta@togglecorp.com>
2025-12-02 12:34:33 +0545

    feat(eap): add export for simplified EAP

---
+++ app/src/components/printable/PrintableContainer/index.tsx
@@ -0,0 +1,43 @@
+import {
+    Heading,
+    type HeadingProps,
+} from '@ifrc-go/ui/printable';
+import { getSpacingValue } from '@ifrc-go/ui/utils';
+
+import styles from './styles.module.css';
+
+interface Props {
+    heading?: React.ReactNode;
+    headingLevel?: HeadingProps['level'];
+    breakBefore?: boolean;
+    breakAfter?: boolean;
+    children?: React.ReactNode;
+}
+
+function PrintableContainer(props: Props) {
+    const {
+        heading,
+        headingLevel = 3,
+        breakAfter,
+        breakBefore,
+        children,
+    } = props;
+
+    const spacing = getSpacingValue('3xl', -headingLevel);
+
+    return (
+        <>
+            {breakBefore && <div className={styles.pageBreak} />}
+            {heading && (
+                <Heading level={headingLevel}>
+                    {heading}
+                </Heading>
+            )}
+            {children}
+            <div style={{ marginBlockEnd: spacing }} />
+            {breakAfter && <div className={styles.pageBreak} />}
+        </>
+    );
+}
+
+export default PrintableContainer;
