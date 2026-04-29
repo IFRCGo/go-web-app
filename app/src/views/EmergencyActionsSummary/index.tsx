5bf264c0e92f1fbcab839d24766205e6fd2f6b81
frozenhelium <ankit.mehta@togglecorp.com>
2026-04-30 01:09:29 +0545

    feat(emergency): add actions taken in emergency actions summary
    - clean-up emergency details page

--- app/src/views/EmergencyActionsSummary/index.tsx
+++ app/src/views/EmergencyActionsSummary/index.tsx
@@ -1,11 +1,92 @@
+import { useOutletContext } from 'react-router-dom';
+import {
+    ButtonLayout,
+    Container,
+    Description,
+    ListView,
+} from '@ifrc-go/ui';
+import { useTranslation } from '@ifrc-go/ui/hooks';
+import {
+    isDefined,
+    isNotDefined,
+} from '@togglecorp/fujs';
+
 import TabPage from '#components/TabPage';
+import { type OrganizationType } from '#utils/constants';
+import { getLatestFieldReport } from '#utils/domain/emergency';
+import { type EmergencyOutletContext } from '#utils/outletContext';
+import { useRequest } from '#utils/restRequest';
+
+import i18n from './i18n.json';
 
 /** @knipignore */
 // eslint-disable-next-line import/prefer-default-export
 export function Component() {
+    const {
+        emergencyResponse,
+        emergencyResponsePending,
+    } = useOutletContext<EmergencyOutletContext>();
+
+    const strings = useTranslation(i18n);
+
+    const latestFieldReportId = getLatestFieldReport(emergencyResponse?.field_reports)?.id;
+
+    const {
+        pending: latestFieldReportPending,
+        response: latestFieldReport,
+    } = useRequest({
+        skip: isNotDefined(latestFieldReportId),
+        url: '/api/v2/field-report/{id}/',
+        pathVariables: isDefined(latestFieldReportId) ? ({
+            id: latestFieldReportId,
+        }) : undefined,
+    });
+
+    const titleByOrganizationMap: Record<OrganizationType, string> = {
+        NTLS: strings.nationalSocietyTitle,
+        FDRN: strings.ifrcTitle,
+        PNS: strings.otherRCRCActorsTitle,
+        GOV: strings.govTitle,
+    };
+
     return (
-        <TabPage>
-            Emergency Actions Summary
+        <TabPage pending={latestFieldReportPending || emergencyResponsePending}>
+            <Container
+                heading={strings.actionsTakenSectionTitle}
+                withHeaderBorder
+            >
+                <ListView layout="block">
+                    {latestFieldReport?.actions_taken?.map((actionTaken) => (
+                        <Container
+                            key={actionTaken.id}
+                            heading={titleByOrganizationMap[actionTaken.organization]}
+                            headingLevel={5}
+                            withContentWell
+                        >
+                            <ListView
+                                layout="block"
+                                spacing="sm"
+                            >
+                                <ListView spacing="sm">
+                                    {actionTaken.actions_details?.map((action) => (
+                                        <ButtonLayout
+                                            // FIXME: use appropriate component
+                                            key={action.id}
+                                            spacingOffset={-3}
+                                            withAdditionalInlinePadding
+                                        >
+                                            {action.name}
+                                        </ButtonLayout>
+                                    ))}
+                                </ListView>
+                                <Description>
+                                    {actionTaken.summary}
+                                </Description>
+                            </ListView>
+                        </Container>
+                    ))}
+                </ListView>
+            </Container>
         </TabPage>
     );
 }
