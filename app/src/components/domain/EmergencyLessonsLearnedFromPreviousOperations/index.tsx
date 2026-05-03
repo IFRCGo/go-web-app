14150b28c6b7cb01424d72b682fcef3e01f3e186
frozenhelium <ankit.mehta@togglecorp.com>
2026-05-03 17:20:57 +0545

    feat(emergency): add lessons learned section in emergency pages
    - Refactor Ops. Learning
      - Rename KeyInsights to OpsLearningKeyInsights
      - Move OpsLearningKeyInsights to components/domain
      - Rename Sources to OpsLearningSources
      - Move OpsLearningSources to compnents/domain
    - Re-use OpsLearningKeyInsights to show lessons learned from previous
      operations in Emergency pages

---
+++ app/src/components/domain/EmergencyLessonsLearnedFromPreviousOperations/index.tsx
@@ -0,0 +1,78 @@
+import { Container } from '@ifrc-go/ui';
+import { useTranslation } from '@ifrc-go/ui/hooks';
+
+import OpsLearningKeyInsights from '#components/domain/OpsLearningKeyInsights';
+import { PER_LEARNING_LESSONS_LEARNED } from '#utils/constants';
+import {
+    SUMMARY_NO_EXTRACT_AVAILABLE,
+    SUMMARY_STATUS_FAILED,
+    SUMMARY_STATUS_SUCCESS,
+} from '#utils/domain/opsLearning';
+import { useRequest } from '#utils/restRequest';
+
+import i18n from './i18n.json';
+
+interface Props {
+    disasterType: number;
+    country: number;
+}
+
+function EmergencyLessonsLearnedFromPreviousOperations(props: Props) {
+    const {
+        disasterType,
+        country,
+    } = props;
+
+    const strings = useTranslation(i18n);
+
+    const {
+        response: summaryResponse,
+        pending: summaryPending,
+    } = useRequest({
+        url: '/api/v2/ops-learning/summary/',
+        query: {
+            appeal_code__country: country,
+            appeal_code__dtype: disasterType,
+            type_validated: PER_LEARNING_LESSONS_LEARNED,
+        },
+        shouldPoll: (poll) => {
+            const { errored, value } = poll;
+
+            const stopPolling = errored
+                || value?.status === SUMMARY_STATUS_FAILED
+                || value?.status === SUMMARY_STATUS_SUCCESS
+                || value?.status === SUMMARY_NO_EXTRACT_AVAILABLE;
+
+            if (stopPolling) {
+                return -1;
+            }
+
+            return 5000;
+        },
+        preserveResponse: true,
+        onFailure: () => {
+            // TODO(frozenhelium): add proper message
+            /*
+            alert.show(
+                strings.failedToFetchSummary,
+                { variant: 'danger' },
+            );
+            */
+        },
+    });
+
+    return (
+        <Container
+            heading={strings.heading}
+            withHeaderBorder
+            pending={summaryPending}
+            // TODO(frozenhelium): show proper message for SUMMARY_STATUS_PENDING
+        >
+            <OpsLearningKeyInsights
+                opsLearningSummaryResponse={summaryResponse}
+            />
+        </Container>
+    );
+}
+
+export default EmergencyLessonsLearnedFromPreviousOperations;
