1a9b44a2556d6bf9ff84124036b58544db889afc
frozenhelium <ankit.mehta@togglecorp.com>
2026-05-03 14:14:42 +0545

    feat(emergency): add timeline view

--- app/src/views/EmergencyDetails/index.tsx
+++ app/src/views/EmergencyDetails/index.tsx
@@ -5,6 +5,7 @@ import {
     Description,
     HtmlOutput,
     InfoPopup,
+    InlineLayout,
     KeyFigureView,
     ListView,
     Message,
@@ -12,6 +13,7 @@ import {
 } from '@ifrc-go/ui';
 import { useTranslation } from '@ifrc-go/ui/hooks';
 import {
+    compareDate,
     isDefined,
     isNotDefined,
     isTruthyString,
@@ -19,11 +21,17 @@ import {
     listToMap,
 } from '@togglecorp/fujs';
 
+import {
+    APPEAL_TYPE_DREF,
+    APPEAL_TYPE_EMERGENCY,
+} from '#components/domain/ActiveOperationMap/utils';
 import SeverityIndicator from '#components/domain/SeverityIndicator';
+import EventTimeline, { type EventTimelineItem } from '#components/EventTimeline';
 import Link from '#components/Link';
 import TabPage from '#components/TabPage';
 import useDisasterType from '#hooks/domain/useDisasterType';
 import useGlobalEnums from '#hooks/domain/useGlobalEnums';
+import { DREF_TYPE_IMMINENT } from '#utils/constants';
 import {
     getFirstFieldReport,
     getLatestAppeal,
@@ -45,6 +53,11 @@ export function Component() {
         emergencyResponse,
         emergencyResponsePending,
         emergencyStage,
+        activeDrefOperation,
+        drefStage,
+        drefApplication,
+        drefOpsUpdate,
+        drefFinalReport: drefFinaReport,
     } = useOutletContext<EmergencyOutletContext>();
     const {
         api_request_choices,
@@ -73,6 +86,7 @@ export function Component() {
         (typeOfDisaster) => typeOfDisaster.id === emergencyResponse?.dtype,
     );
 
+    // FIXME(frozenhelium): verify if this is correct
     const mdrCode = isDefined(emergencyResponse)
         && isDefined(emergencyResponse?.appeals)
         && emergencyResponse.appeals.length > 0
@@ -82,6 +96,7 @@ export function Component() {
     const assistanceIsRequestedByNS = firstFieldReport?.ns_request_assistance;
     const assistanceIsRequestedByCountry = firstFieldReport?.request_assistance;
     const latestFieldReport = getLatestFieldReport(emergencyResponse?.field_reports);
+    const latestAppeal = getLatestAppeal(emergencyResponse?.appeals);
 
     const {
         pending: latestFullFieldReportPending,
@@ -132,6 +147,161 @@ export function Component() {
         [emergencyContacts, latestFieldReport],
     );
 
+    const timelineEvents = useMemo(
+        () => {
+            const events: EventTimelineItem[] = [];
+
+            if (isDefined(firstFieldReport)
+                && firstFieldReport?.id !== latestFieldReport?.id
+                && isDefined(firstFieldReport.report_date)
+            ) {
+                events.push({
+                    key: `fr-${firstFieldReport.id}`,
+                    date: new Date(firstFieldReport.report_date),
+                    // FIXME: use strings
+                    label: 'First Field Report',
+                });
+            }
+
+            if (isDefined(latestFieldReport) && isDefined(latestFieldReport.report_date)) {
+                events.push({
+                    key: `fr-${latestFieldReport.id}`,
+                    date: new Date(latestFieldReport.report_date),
+                    // FIXME: use strings
+                    label: 'Last Field Report',
+                });
+            }
+
+            if (latestAppeal?.atype === APPEAL_TYPE_EMERGENCY) {
+                if (isDefined(emergencyResponse)
+                    && isDefined(emergencyResponse.disaster_start_date)
+                ) {
+                    events.push({
+                        key: `ea-start-${emergencyResponse.id}`,
+                        date: new Date(emergencyResponse.disaster_start_date),
+                        // FIXME: use strings
+                        label: 'Beginning of the Disaster',
+                    });
+                }
+
+                if (isDefined(latestAppeal.start_date)) {
+                    events.push({
+                        key: `ea-start-${latestAppeal.id}`,
+                        date: new Date(latestAppeal.start_date),
+                        // FIXME: use strings
+                        label: 'Start of the Operation',
+                    });
+                }
+
+                if (isDefined(latestAppeal.end_date)) {
+                    events.push({
+                        key: `ea-end-${latestAppeal.id}`,
+                        date: new Date(latestAppeal.end_date),
+                        // FIXME: use strings
+                        label: 'End of the Operation',
+                    });
+                }
+            }
+
+            if (latestAppeal?.atype === APPEAL_TYPE_DREF && isDefined(activeDrefOperation)) {
+                const {
+                    id,
+                    date_of_approval,
+                    has_ops_update,
+                    operational_update_details,
+                    has_final_report,
+                    final_report_details,
+                } = activeDrefOperation;
+
+                if (isDefined(drefApplication)) {
+                    const {
+                        type_of_dref,
+                        event_date,
+                        hazard_date,
+                    } = drefApplication;
+
+                    if (type_of_dref !== DREF_TYPE_IMMINENT && isDefined(event_date)) {
+                        events.push({
+                            key: `dref-application-event-start-${id}`,
+                            date: new Date(event_date),
+                            // FIXME: use strings
+                            label: 'Beginning of the Disaster',
+                        });
+                    }
+
+                    if (type_of_dref === DREF_TYPE_IMMINENT && isDefined(hazard_date)) {
+                        events.push({
+                            key: `dref-application-event-start-${id}`,
+                            date: new Date(hazard_date),
+                            // FIXME: use strings
+                            label: 'Beginning of the Disaster',
+                        });
+                    }
+                }
+
+                if (isDefined(date_of_approval)) {
+                    events.push({
+                        key: `dref-application-approved-${id}`,
+                        date: new Date(date_of_approval),
+                        // FIXME: use strings
+                        label: 'Start of Operation',
+                    });
+                }
+
+                if (has_ops_update && operational_update_details.length > 0) {
+                    operational_update_details.toReversed().forEach((opsUpdate, i) => {
+                        // FIXME(frozenhelium): we need date_of_approval or updated_at here
+                        if (isDefined(opsUpdate.created_at)) {
+                            events.push({
+                                key: `ops-update-${opsUpdate.id}`,
+                                date: new Date(opsUpdate.created_at),
+                                // FIXME: use strings
+                                label: `Operational Update #${i + 1}`,
+                            });
+                        }
+                    });
+                }
+
+                if (has_final_report
+                    && isDefined(final_report_details)
+                    && final_report_details.created_at
+                ) {
+                    // FIXME(frozenhelium): we need date_of_approval or updated_at here
+                    events.push({
+                        key: `final-report-${final_report_details.id}`,
+                        date: new Date(final_report_details.created_at),
+                        // FIXME: use strings
+                        label: 'DREF Final Report',
+                    });
+                }
+
+                const endDate = drefFinaReport?.operation_end_date
+                    ?? drefOpsUpdate?.new_operational_end_date
+                    ?? drefApplication?.end_date;
+
+                if (isDefined(endDate)) {
+                    events.push({
+                        key: 'end-of-dref-operations',
+                        date: new Date(endDate),
+                        label: 'End of Operation',
+                    });
+                }
+            }
+
+            return events.toSorted((a, b) => compareDate(a.date, b.date));
+        },
+        [
+            latestAppeal,
+            emergencyResponse,
+            firstFieldReport,
+            latestFieldReport,
+            activeDrefOperation,
+            drefApplication,
+            drefOpsUpdate,
+            drefFinaReport,
+        ],
+    );
+
     if (isNotDefined(emergencyResponse)) {
         return (
             <Message
@@ -142,47 +312,70 @@ export function Component() {
 
     return (
         <TabPage pending={latestFullFieldReportPending}>
-            <Container
-                heading={strings.emergencyKeyFiguresTitle}
-                withHeaderBorder
-            >
-                <ListView
-                    layout="grid"
-                    numPreferredGridColumns={5}
-                    spacing="sm"
+            {(emergencyStage === 'field-report' || emergencyStage === 'emergency-appeal') && (
+                <Container
+                    heading={strings.emergencyKeyFiguresTitle}
+                    withHeaderBorder
                 >
-                    <KeyFigureView
-                        label={strings.keyFigureInjuredLabel}
-                        value={latestFieldReport?.num_injured}
-                        valueType="number"
-                        withShadow
-                    />
-                    <KeyFigureView
-                        label={strings.keyFigureDeadLabel}
-                        value={latestFieldReport?.num_dead}
-                        valueType="number"
-                        withShadow
-                    />
-                    <KeyFigureView
-                        label={strings.keyFigureMissingLabel}
-                        value={latestFieldReport?.num_missing}
-                        valueType="number"
-                        withShadow
-                    />
-                    <KeyFigureView
-                        label={strings.keyFigureAffectedLabel}
-                        value={latestFieldReport?.num_affected}
-                        valueType="number"
-                        withShadow
-                    />
-                    <KeyFigureView
-                        label={strings.keyFigureDisplacedLabel}
-                        value={latestFieldReport?.num_displaced}
-                        valueType="number"
-                        withShadow
-                    />
-                </ListView>
-            </Container>
+                    <ListView
+                        layout="grid"
+                        numPreferredGridColumns={5}
+                        spacing="sm"
+                    >
+                        <KeyFigureView
+                            label={strings.keyFigureInjuredLabel}
+                            value={latestFieldReport?.num_injured}
+                            valueType="number"
+                            withShadow
+                        />
+                        <KeyFigureView
+                            label={strings.keyFigureDeadLabel}
+                            value={latestFieldReport?.num_dead}
+                            valueType="number"
+                            withShadow
+                        />
+                        <KeyFigureView
+                            label={strings.keyFigureMissingLabel}
+                            value={latestFieldReport?.num_missing}
+                            valueType="number"
+                            withShadow
+                        />
+                        <KeyFigureView
+                            label={strings.keyFigureAffectedLabel}
+                            value={latestFieldReport?.num_affected}
+                            valueType="number"
+                            withShadow
+                        />
+                        <KeyFigureView
+                            label={strings.keyFigureDisplacedLabel}
+                            value={latestFieldReport?.num_displaced}
+                            valueType="number"
+                            withShadow
+                        />
+                    </ListView>
+                </Container>
+            )}
+            {emergencyStage !== 'field-report' && (
+                <InlineLayout
+                    after={(
+                        <ListView
+                            layout="block"
+                            withSpacingOpticalCorrection
+                        >
+                            <TextOutput
+                                label={strings.MDRCode}
+                                value={mdrCode}
+                                strongValue
+                            />
+                            <TextOutput
+                                label={strings.GLIDENumber}
+                                value={emergencyResponse?.glide}
+                                strongValue
+                            />
+                        </ListView>
+                    )}
+                />
+            )}
             <Container
                 heading={strings.emergencyOverviewTitle}
                 withHeaderBorder
@@ -190,7 +383,7 @@ export function Component() {
                 <ListView
                     layout="grid"
                     withSpacingOpticalCorrection
-                    numPreferredGridColumns={3}
+                    numPreferredGridColumns={emergencyStage === 'dref' ? 2 : 3}
                 >
                     <ListView layout="block">
                         <TextOutput
@@ -203,21 +396,16 @@ export function Component() {
                             value={disasterType?.name}
                             strongValue
                         />
-                        {emergencyStage === 'emergency-appeal' && (
+                        {emergencyStage !== 'field-report' && (
                             <>
                                 <TextOutput
                                     label={strings.overviewOperationTypeLabel}
-                                    value={getLatestAppeal(emergencyResponse.appeals)
-                                        ?.atype_display}
-                                />
-                                <TextOutput
-                                    label={strings.MDRCode}
-                                    value={mdrCode}
-                                    strongValue
-                                />
-                                <TextOutput
-                                    label={strings.GLIDENumber}
-                                    value={emergencyResponse?.glide}
+                                    value={[
+                                        latestAppeal?.atype_display,
+                                        latestAppeal?.atype === APPEAL_TYPE_DREF && drefStage === 'application' && drefApplication?.type_of_dref_display,
+                                        latestAppeal?.atype === APPEAL_TYPE_DREF && drefStage === 'ops-update' && drefOpsUpdate?.type_of_dref_display,
+                                        latestAppeal?.atype === APPEAL_TYPE_DREF && drefStage === 'final-report' && drefFinaReport?.type_of_dref_display,
+                                    ].filter(Boolean).join(', ')}
                                     strongValue
                                 />
                                 <TextOutput
@@ -226,12 +414,12 @@ export function Component() {
                                         <ListView
                                             withWrap
                                             withSpacingOpticalCorrection
-                                            spacing="sm"
+                                            spacing="2xs"
                                         >
-                                            {emergencyResponse.ifrc_severity_level_display}
                                             <SeverityIndicator
                                                 level={emergencyResponse.ifrc_severity_level}
                                             />
+                                            {emergencyResponse.ifrc_severity_level_display}
                                             {emergencyResponse.ifrc_severity_level_update_date && (
                                                 <InfoPopup
                                                     description={(
@@ -287,35 +475,56 @@ export function Component() {
                             strongValue
                         />
                     </ListView>
-                    <ListView layout="block">
-                        <TextOutput
-                            label={strings.assistanceRequestedByGovernment}
-                            valueType="boolean"
-                            value={assistanceIsRequestedByCountry}
-                            strongValue
-                        />
-                        <TextOutput
-                            label={strings.assistanceRequestedByNS}
-                            valueType="boolean"
-                            value={assistanceIsRequestedByNS}
-                            strongValue
-                        />
-                    </ListView>
+                    {emergencyStage !== 'dref' && (
+                        <ListView layout="block">
+                            <TextOutput
+                                label={strings.assistanceRequestedByGovernment}
+                                valueType="boolean"
+                                value={assistanceIsRequestedByCountry}
+                                strongValue
+                            />
+                            <TextOutput
+                                label={strings.assistanceRequestedByNS}
+                                valueType="boolean"
+                                value={assistanceIsRequestedByNS}
+                                strongValue
+                            />
+                        </ListView>
+                    )}
                 </ListView>
             </Container>
+
+            {isDefined(timelineEvents) && timelineEvents.length > 0 && (
+                <Container
+                    heading={strings.operationalTimelineTitle}
+                    withHeaderBorder
+                >
+                    <EventTimeline
+                        events={timelineEvents}
+                    />
+                </Container>
+            )}
             <Container
                 heading={strings.situationalOverviewTitle}
                 withHeaderBorder
             >
                 {/* FIXME(frozenhelium): handle condition where there is no summary */}
                 <ListView layout="grid">
-                    <HtmlOutput
-                        value={emergencyResponse.summary}
-                    />
+                    {emergencyStage === 'dref' && (
+                        <Description>
+                            {drefApplication?.event_scope}
+                        </Description>
+                    )}
+                    {emergencyStage !== 'dref' && (
+                        <HtmlOutput
+                            value={emergencyResponse.summary}
+                        />
+                    )}
                     <EmergencyMap event={emergencyResponse} />
                 </ListView>
             </Container>
             <Container
+                // TODO(frozenhelium): use separate component, use translations
                 heading="Lessons learned from previous operations"
                 withHeaderBorder
                 empty
